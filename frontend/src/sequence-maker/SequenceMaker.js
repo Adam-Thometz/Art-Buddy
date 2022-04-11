a69a5cf16a5fbfc26f3b57ed8afdc4dc575ffb12
Adam Thometz <adamthometz@gmail.com>
2022-04-11 14:33:57 -0400

    added basic styling to sequence maker

--- frontend/src/sequence-maker/SequenceMaker.js
+++ frontend/src/sequence-maker/SequenceMaker.js
@@ -2,6 +2,8 @@ import React from "react";
 
 import { useSelector } from "react-redux";
 
+import './SequenceMaker.css'
+
 import Categories from "./categories/Categories";
 import Options from "./options/Options";
 import Sequence from "./sequence/Sequence";
@@ -11,8 +13,8 @@ const SequenceMaker = () => {
   return (
     <div className="SequenceMaker">
       <Categories />
-      {category.length ? <Options /> : null}
-      {category.length ? <Sequence /> : null}
+      {category ? <Options /> : null}
+      {category ? <Sequence /> : null}
     </div>
   );
 };
