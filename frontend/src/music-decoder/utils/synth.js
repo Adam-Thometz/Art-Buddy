c033ad425af894684210cd03bc9e7144e0b6b38a
Adam Thometz <adamthometz@gmail.com>
2022-04-11 12:09:52 -0400

    changed sound for decoder

--- frontend/src/music-decoder/utils/synth.js
+++ frontend/src/music-decoder/utils/synth.js
@@ -1,5 +1,7 @@
 import * as Tone from 'tone';
 
-const synth = new Tone.Synth().toDestination()
+const bitcrusher = new Tone.BitCrusher(4).toDestination()
+// const filter = new Tone.AutoFilter()
+const synth = new Tone.Synth().connect(bitcrusher)
 
 export default synth;
