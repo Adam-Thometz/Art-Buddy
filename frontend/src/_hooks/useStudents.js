9fc75385897bf38fcce55a8a411b53482b3bc790
Adam Thometz <adamthometz@gmail.com>
2022-04-13 12:10:00 -0400

    added custom hooks folder

---
+++ frontend/src/_hooks/useStudents.js
@@ -0,0 +1,18 @@
+import { useState, useEffect } from 'react';
+
+const useLocalStorage = (key, defaultValue = null) => {
+  const initialValue = localStorage.getItem(key) || defaultValue;
+  const [state, setState] = useState(initialValue);
+
+  useEffect(() => {
+    if (state === null) {
+      localStorage.removeItem(key);
+    } else {
+      localStorage.setItem(key, state);
+    }
+  }, [key, state]);
+
+  return [state, setState];
+}
+
+export default useLocalStorage;
