45ec98cfc989076501556bb3f25cee99d0afe69d
Adam Thometz <adamthometz@gmail.com>
2022-04-14 09:11:58 -0400

    added styles to welcome page

--- frontend/src/landing-page/Welcome.js
+++ frontend/src/landing-page/Welcome.js
@@ -8,9 +8,10 @@ const Welcome = ({ setHasVisited }) => {
   }
   return (
     <div className="Welcome">
-      <h1>Welcome to Art Buddy!</h1>
+      <h1>Welcome to ARTBUDDY!</h1>
       <div className="Welcome-description">
         <p>Art Buddy is a collection of fun and interactive fine arts games designed specifically with special education goals in mind</p>
+        <p>Click below to get started</p>
         <button onClick={handleVisited}>Let's Go!</button>
       </div>
     </div>
