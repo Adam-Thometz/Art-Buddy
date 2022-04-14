573b9d22ce0ac49fd522a3ce8392c730273a431f
Adam Thometz <adamthometz@gmail.com>
2022-04-14 12:25:28 -0400

    created button component

--- frontend/src/landing-page/Welcome.js
+++ frontend/src/landing-page/Welcome.js
@@ -2,6 +2,8 @@ import React from "react";
 
 import './Welcome.css';
 
+import Button from "../_common/Button";
+
 const Welcome = ({ setHasVisited }) => {
   const handleVisited = () => {
     setHasVisited(true);
@@ -10,9 +12,9 @@ const Welcome = ({ setHasVisited }) => {
     <div className="Welcome">
       <h1>Welcome to ARTBUDDY!</h1>
       <div className="Welcome-description">
-        <p>Art Buddy is a collection of fun and interactive fine arts games designed specifically with special education goals in mind</p>
+        <p>ARTBUDDY is a collection of fun and interactive fine arts games designed specifically with special education goals in mind</p>
         <p>Click below to get started</p>
-        <button onClick={handleVisited}>Let's Go!</button>
+        <Button outlineColor={'#89EB0A'} onClick={handleVisited} text={"Let's Go!"} />
       </div>
     </div>
   );
