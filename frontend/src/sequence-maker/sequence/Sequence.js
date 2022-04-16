e32a0679d3e31f61c1d554e7a3f038f8221c9ddc
Adam Thometz <adamthometz@gmail.com>
2022-04-16 14:28:35 -0400

    fixed frontend bug in sequence maker

--- frontend/src/sequence-maker/sequence/Sequence.js
+++ frontend/src/sequence-maker/sequence/Sequence.js
@@ -10,12 +10,12 @@ const Sequence = () => {
   const sequence = useSelector(state => state.sequenceMaker.sequence)
   return (
     <div className="Sequence">
+      {sequence.some(block => block !== null) ? <p>Click on a picture to hear the sound!</p> : null}
       <div className="Sequence-display">
         {sequence.map(block => (
           <SequenceBlock block={block} />
         ))}
       </div>
-      {sequence.some(block => block !== null) ? "Click on a picture to hear the sound!" : null}
     </div>
   );
 };
