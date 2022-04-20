733ae676d1c97ffc5e62dd7d3c4f37ab146500f2
Adam Thometz <adamthometz@gmail.com>
2022-04-20 10:57:33 -0400

    refactored window component

--- frontend/src/_components/window/Window.js
+++ frontend/src/_components/window/Window.js
@@ -2,9 +2,9 @@ import React from "react";
 
 import './Window.css';
 
-const Window = ({ children, customStyles = {} }) => {
+const Window = ({ children, smallWindow = false }) => {
   return (
-    <div className="Window" style={customStyles}>
+    <div className="Window" style={{ width: smallWindow ? '70%' : '90%' }}>
       {children}
     </div>
   );
