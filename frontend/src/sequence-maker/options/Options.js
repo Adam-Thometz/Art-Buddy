bc3b26e1013a890fe8c4f3971d709f6b10439253
Adam Thometz <adamthometz@gmail.com>
2022-04-20 20:19:39 -0400

    rearranged add/remove block buttons

--- frontend/src/sequence-maker/options/Options.js
+++ frontend/src/sequence-maker/options/Options.js
@@ -1,13 +1,10 @@
 import React from "react";
 
 import { useSelector, useDispatch } from "react-redux";
-import { addToSequence, addBlock, removeBlock } from "../../_redux/actions/actions";
+import { addToSequence } from "../../_redux/actions/actions";
 
 import './Options.css';
 
-import Button from "../../_components/button/Button";
-
-import colors from "../../_components/button/colorOrder";
 import soundInfo from "../_media/soundInfo";
 
 const Options = () => {
@@ -20,24 +17,14 @@ const Options = () => {
     if (sequence.some(block => !block)) dispatch(addToSequence(e.target.classList[1]));
   }
 
-  const add = () => {
-    dispatch(addBlock());
-  }
-  
-  const remove = () => {
-    dispatch(removeBlock());
-  }
-
   return (
     <div className="Options">
-      <Button small borderColor={colors[2]} onClick={remove}>REMOVE BLOCK</Button>
       {Object.keys(options).map(option => (
         <div className="Options-option">
           <img className={`Options-img ${option}`} src={options[option].image} alt={options[option].alt} onClick={addOption} />
           <p className="Options-description">{options[option].alt}</p>
         </div>
       ))}
-      <Button small borderColor={colors[3]} onClick={add}>ADD BLOCK</Button>
     </div>
   );
 };
--- frontend/src/sequence-maker/sequence/Sequence.js
+++ frontend/src/sequence-maker/sequence/Sequence.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { playAllSequence, playSequence, resetSequence } from "../../_redux/actions/actions";
+import { playAllSequence, playSequence, resetSequence, addBlock, removeBlock } from "../../_redux/actions/actions";
 import { useSelector, useDispatch } from "react-redux";
 
 import './Sequence.css';
@@ -12,7 +12,15 @@ import colors from "../../_components/button/colorOrder";
 
 const Sequence = () => {
   const sequence = useSelector(state => state.sequenceMaker.sequence);
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
+
+  const add = () => {
+    dispatch(addBlock());
+  }
+  
+  const remove = () => {
+    dispatch(removeBlock());
+  }
 
   const handlePlay = () => {
     dispatch(playSequence())
@@ -28,7 +36,11 @@ const Sequence = () => {
   
   return (
     <div className="Sequence">
-      {sequence.some(block => block !== null) ? <p>CLICK ON A PICTURE TO HEAR THE SOUND.</p> : null}
+      <div className="Sequence-header">
+        <Button small borderColor={colors[2]} onClick={remove}>REMOVE BLOCK</Button>
+        {sequence.some(block => block !== null) ? <p>CLICK ON A PICTURE TO HEAR THE SOUND.</p> : null}
+        <Button small borderColor={colors[3]} onClick={add}>ADD BLOCK</Button>
+      </div>
       <div className="Sequence-display">
         {sequence.map((block, i) => (
           <SequenceBlock block={block} borderColor={colors[i%4]} />
