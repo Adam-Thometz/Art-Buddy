742978d88dbd3da087aba93fa56d968453b089ac
Adam Thometz <adamthometz@gmail.com>
2022-04-21 17:40:48 -0400

    refactored decoder to include dropdown component

--- frontend/src/music-decoder/music-controls/MusicControls.js
+++ frontend/src/music-decoder/music-controls/MusicControls.js
@@ -1,16 +1,27 @@
 import React from "react";
 
 import { useDispatch } from "react-redux";
+import Dropdown from "../../_components/dropdown/Dropdown";
 import { changeScale } from "../../_redux/actions/actions";
+
+import scales from "../_utils/scales";
 
 const MusicControls = () => {
   const dispatch = useDispatch();
+
   const handleChangeScale = e => {
-    dispatch(changeScale(e.target.value))
+    dispatch(changeScale(e.target.value));
   };
+
   return (
     <div className="MusicControls">
-      <label>SELECT SCALE: </label>
+      <Dropdown
+        labelText='SELECT SCALE'
+        onChange={handleChangeScale}
+        options={scales}
+        defaultValue={0}
+      />
+      {/* <label className="MusicControls-label">SELECT SCALE: </label>
       <select defaultValue={0} onChange={handleChangeScale}>
         <option value={0}>C</option>
         <option value={1}>C# / Db</option>
@@ -24,9 +35,9 @@ const MusicControls = () => {
         <option value={9}>A</option>
         <option value={10}>A# / Bb</option>
         <option value={11}>B</option>
-      </select>
+      </select> */}
     </div>
   );
 };
 
-export default MusicControls
+export default MusicControls;
