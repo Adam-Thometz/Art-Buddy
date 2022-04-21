0131cdc965bdba23b4ff8b2239ed125c2e28dfaf
Adam Thometz <adamthometz@gmail.com>
2022-04-20 20:11:15 -0400

    added different styles for small button

--- frontend/src/_components/button/Button.js
+++ frontend/src/_components/button/Button.js
@@ -2,15 +2,23 @@ import React from "react";
 
 import './Button.css'
 
-const Button = ({ children, borderColor, onClick, small, icon }) => {
+const Button = ({ children, borderColor, onClick, small = false, selected = false, icon = null }) => {
   const styles = {
     borderColor,
-    fontSize: small ? '1rem' : '2rem'
+    fontSize: small ? '1rem' : '2rem',
+    border: small ? '5px solid' : '15px solid',
+    margin: small ? '2%' : '5%',
+    height: small ? '25px' : null,
+    padding: small ? '2%' : '5%',
+    color: selected ? 'white' : 'black',
+    backgroundColor: selected ? 'black' : 'white'
   }
+
   return (
-    <div className="Button" onClick={onClick} style={styles}>
+    <span className="Button" onClick={onClick} style={styles}>
+      {icon ? <img src={icon} alt='' /> : null}
       {children}
-    </div>
+    </span>
   );
 };
 
