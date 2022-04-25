69137d320aa7d4a1cf00638a38790f8470d3e741
Adam Thometz <adamthometz@gmail.com>
2022-04-25 14:22:50 -0400

    updated components

--- frontend/src/_components/clickable-icon/Clickable.js
+++ frontend/src/_components/clickable-icon/Clickable.js
@@ -2,10 +2,15 @@ import React from "react";
 
 import './Clickable.css';
 
-const Clickable = ({ icon, text, onClick }) => {
+const Clickable = ({ icon, text, onClick, size = "250px", additionalClass = null }) => {
+  const sizeStyle = {
+    width: size,
+    height: size
+  };
+
   return (
-    <div className="Clickable" onClick={onClick}>
-      <img src={icon} alt="" />
+    <div className={`Clickable${additionalClass ? " " + additionalClass : null}`} onClick={onClick}>
+      <img style={sizeStyle} src={icon} alt="" />
       <p>{text}</p>
     </div>
   );
---
+++ frontend/src/_components/option/Options.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import './Options.css'
+
+const Options = ({ children, width = '100%' }) => {
+  return (
+    <div className="Options" style={{width}}>
+      {children}
+    </div>
+  );
+};
+
+export default Options;
