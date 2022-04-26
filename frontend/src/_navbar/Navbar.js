deed9a56eceaebbae8e5df7aca86674d786aac3c
Adam Thometz <adamthometz@gmail.com>
2022-04-26 07:51:33 -0400

    removed login feature

--- frontend/src/_navbar/Navbar.js
+++ frontend/src/_navbar/Navbar.js
@@ -1,20 +1,13 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
-// import { useSelector } from "react-redux";
-
 import './Navbar.css';
-
-// import LoggedIn from "./LoggedIn";
-// import LoggedOut from "./LoggedOut";
 
 const Navbar = () => {
   const navigate = useNavigate()
-  // const user = useSelector(state => state.userInfo.user);
   return (
     <div className="Navbar">
-      <h1 className="Navbar-header" onClick={() => navigate('/')}>ARTBUDDY</h1>
-      {/* {user.username ? <LoggedIn /> : <LoggedOut />} */}
+      <span className="Navbar-header" onClick={() => navigate('/')}>ARTBUDDY</span>
     </div>
   );
 };
