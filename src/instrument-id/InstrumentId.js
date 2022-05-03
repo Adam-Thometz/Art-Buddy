4a49287da87c3278651c4393969022a771174a6b
Adam Thometz <adamthometz@gmail.com>
2022-05-03 09:25:50 -0400

    took designer feedback on design of instrument id page

--- src/instrument-id/InstrumentId.js
+++ src/instrument-id/InstrumentId.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
+import './InstrumentId.css';
+
 import Icon from "../_components/icon/Icon";
 import Options from "../_components/option/Options";
 
@@ -11,14 +13,11 @@ const InstrumentId = () => {
   const navigate = useNavigate();
   return (
     <div className="InstrumentId">
-      <div className="InstrumentId">
-        <h1>MAIN MENU</h1>
-        <p>Let's learn about different instruments! Click PLAY to play different instruments and click LEARN to learn about and listen to different instruments</p>
-        <Options width="60%">
-          <Icon icon={playIcon} text="PLAY" onClick={() => navigate(instrumentIdUrls.playUrl)} />
-          <Icon icon={learnIcon} text="LEARN" onClick={() => navigate(instrumentIdUrls.learnUrl)} />
-        </Options>
-      </div>
+      <p className="InstrumentId-instructions">Let's learn about instruments! Click LEARN to review your instrument knowledge. Click PLAY to test your skills</p>
+      <Options width="60%">
+        <Icon icon={learnIcon} text="LEARN" onClick={() => navigate(instrumentIdUrls.learnUrl)} />
+        <Icon icon={playIcon} text="PLAY" onClick={() => navigate(instrumentIdUrls.playUrl)} />
+      </Options>
     </div>
   );
 };
--- src/instrument-id/learn/learnInstrumentOptions.js
+++ src/instrument-id/learn/learnInstrumentOptions.js
@@ -16,28 +16,32 @@ const learnInstrumentOptions = {
     instruments: [{
         name: 'trumpet',
         icon: icons.trumpetIcon,
-        description: 'lorum ipsum dolor and all that',
+        madeFrom: 'lorum ipsum dolor and all that',
+        howToPlay: '',
         sound: sample(sounds.trumpet),
         videoUrl: ''
       },
       {
         name: 'french horn',
         icon: icons.frenchHornIcon,
-        description: 'lorum ipsum dolor and all that',
+        madeFrom: 'lorum ipsum dolor and all that',
+        howToPlay: '',
         sound: sample(sounds.frenchHorn),
         videoUrl: ''
       },
       {
         name: 'trombone',
         icon: icons.tromboneIcon,
-        description: 'lorum ipsum dolor and all that',
+        madeFrom: 'lorum ipsum dolor and all that',
+        howToPlay: '',
         sound: sample(sounds.trombone),
         videoUrl: ''
       },
       {
         name: 'tuba',
         icon: icons.tubaIcon,
-        description: 'lorum ipsum dolor and all that',
+        madeFrom: 'lorum ipsum dolor and all that',
+        howToPlay: '',
         sound: sample(sounds.tuba),
         videoUrl: ''
       },
@@ -49,35 +53,40 @@ const learnInstrumentOptions = {
       {
         name: 'electric guitar',
         icon: icons.electricGuitarIcon,
-        description: 'lorum ipsum dolor and all that',
+        madeFrom: 'lorum ipsum dolor and all that',
+        howToPlay: '',
         sound: sample(sounds.electricGuitar),
         videoUrl: ''
       },
       {
         name: 'electric bass',
         icon: icons.electricBassIcon,
-        description: 'lorum ipsum dolor and all that',
+        madeFrom: 'lorum ipsum dolor and all that',
+        howToPlay: '',
         sound: sample(sounds.electricBass),
         videoUrl: ''
       },
       {
         name: 'synthesizer',
         icon: icons.synthIcon,
-        description: 'lorum ipsum dolor and all that',
+        madeFrom: 'lorum ipsum dolor and all that',
+        howToPlay: '',
         sound: sample(sounds.synthesizer),
         videoUrl: ''
       },
       {
         name: 'sampler',
         icon: icons.samplerIcon,
-        description: 'lorum ipsum dolor and all that',
+        madeFrom: 'lorum ipsum dolor and all that',
+        howToPlay: '',
         sound: sample(sounds.sampler),
         videoUrl: ''
       },
       {
         name: 'theremin',
         icon: icons.thereminIcon,
-        description: 'lorum ipsum dolor and all that',
+        madeFrom: 'lorum ipsum dolor and all that',
+        howToPlay: '',
         sound: sample(sounds.theremin),
         videoUrl: ''
       },
@@ -89,42 +98,48 @@ const learnInstrumentOptions = {
       {
         name: 'djembe',
         icon: icons.djembeIcon,
-        description: 'lorum ipsum dolor and all that',
+        madeFrom: 'lorum ipsum dolor and all that',
+        howToPlay: '',
         sound: sample(sounds.djembe),
         videoUrl: ''
       },
       {
         name: 'drum set',
         icon: icons.drumSetIcon,
-        description: 'lorum ipsum dolor and all that',
+        madeFrom: 'lorum ipsum dolor and all that',
+        howToPlay: '',
         sound: sample(sounds.drumSet),
         videoUrl: ''
       },
       {
         name: 'tambourine',
         icon: icons.tambourineIcon,
-        description: 'lorum ipsum dolor and all that',
+        madeFrom: 'lorum ipsum dolor and all that',
+        howToPlay: '',
         sound: sample(sounds.tambourine),
         videoUrl: ''
       },
       {
         name: 'xylophone',
         icon: icons.xylophoneIcon,
-        description: 'lorum ipsum dolor and all that',
+        madeFrom: 'lorum ipsum dolor and all that',
+        howToPlay: '',
         sound: sample(sounds.xylophone),
         videoUrl: ''
       },
       {
         name: 'triangle',
         icon: icons.triangleIcon,
-        description: 'lorum ipsum dolor and all that',
+        madeFrom: 'lorum ipsum dolor and all that',
+        howToPlay: '',
         sound: sample(sounds.triangle),
         videoUrl: ''
       },
       {
         name: 'conga',
         icon: icons.congaIcon,
-        description: 'lorum ipsum dolor and all that',
+        madeFrom: 'lorum ipsum dolor and all that',
+        howToPlay: '',
         sound: sample(sounds.conga),
         videoUrl: ''
       },
@@ -136,35 +151,40 @@ const learnInstrumentOptions = {
       {
         name: 'acoustic guitar',
         icon: icons.acousticGuitarIcon,
-        description: 'lorum ipsum dolor and all that',
+        madeFrom: 'lorum ipsum dolor and all that',
+        howToPlay: '',
         sound: sample(sounds.acousticGuitar),
         videoUrl: ''
       },
       {
         name: 'banjo',
         icon: icons.banjoIcon,
-        description: 'lorum ipsum dolor and all that',
+        madeFrom: 'lorum ipsum dolor and all that',
+        howToPlay: '',
         sound: sample(sounds.banjo),
         videoUrl: ''
       },
       {
         name: 'harp',
         icon: icons.harpIcon,
-        description: 'lorum ipsum dolor and all that',
+        madeFrom: 'lorum ipsum dolor and all that',
+        howToPlay: '',
         sound: sample(sounds.harp),
         videoUrl: ''
       },
       {
         name: 'piano',
         icon: icons.pianoIcon,
-        description: 'lorum ipsum dolor and all that',
+        madeFrom: 'lorum ipsum dolor and all that',
+        howToPlay: '',
         sound: sample(sounds.piano),
         videoUrl: ''
       },
       {
         name: 'violin',
         icon: icons.violinIcon,
-        description: 'lorum ipsum dolor and all that',
+        madeFrom: 'lorum ipsum dolor and all that',
+        howToPlay: '',
         sound: sample(sounds.violin),
         videoUrl: ''
       },
@@ -176,35 +196,40 @@ const learnInstrumentOptions = {
       {
         name: 'clarinet',
         icon: icons.clarinetIcon,
-        description: 'lorum ipsum dolor and all that',
+        madeFrom: 'lorum ipsum dolor and all that',
+        howToPlay: '',
         sound: sample(sounds.clarinet),
         videoUrl: ''
       },
       {
         name: 'bassoon',
         icon: icons.bassoonIcon,
-        description: 'lorum ipsum dolor and all that',
+        madeFrom: 'lorum ipsum dolor and all that',
+        howToPlay: '',
         sound: sample(sounds.bassoon),
         videoUrl: ''
       },
       {
         name: 'flute',
         icon: icons.fluteIcon,
-        description: 'lorum ipsum dolor and all that',
+        madeFrom: 'lorum ipsum dolor and all that',
+        howToPlay: '',
         sound: sample(sounds.flute),
         videoUrl: ''
       },
       {
         name: 'saxophone',
         icon: icons.saxophoneIcon,
-        description: 'lorum ipsum dolor and all that',
+        madeFrom: 'lorum ipsum dolor and all that',
+        howToPlay: '',
         sound: sample(sounds.saxophone),
         videoUrl: ''
       },
       {
         name: 'pan flute',
         icon: icons.panFluteIcon,
-        description: 'lorum ipsum dolor and all that',
+        madeFrom: 'lorum ipsum dolor and all that',
+        howToPlay: '',
         sound: sample(sounds.panFlute),
         videoUrl: ''
       },
@@ -216,42 +241,48 @@ const learnInstrumentOptions = {
       {
         name: 'soprano',
         icon: icons.sopranoIcon,
-        description: 'lorum ipsum dolor and all that',
+        madeFrom: 'lorum ipsum dolor and all that',
+        howToPlay: '',
         sound: sample(sounds.soprano),
         videoUrl: ''
       },
       {
         name: 'mezzo soprano',
         icon: icons.mezzoSopranoIcon,
-        description: 'lorum ipsum dolor and all that',
+        madeFrom: 'lorum ipsum dolor and all that',
+        howToPlay: '',
         sound: sample(sounds.mezzoSoprano),
         videoUrl: ''
       },
       {
         name: 'alto',
         icon: icons.altoIcon,
-        description: 'lorum ipsum dolor and all that',
+        madeFrom: 'lorum ipsum dolor and all that',
+        howToPlay: '',
         sound: sample(sounds.alto),
         videoUrl: ''
       },
       {
         name: 'tenor',
         icon: icons.tenorIcon,
-        description: 'lorum ipsum dolor and all that',
+        madeFrom: 'lorum ipsum dolor and all that',
+        howToPlay: '',
         sound: sample(sounds.tenor),
         videoUrl: ''
       },
       {
         name: 'baritone',
         icon: icons.baritoneIcon,
-        description: 'lorum ipsum dolor and all that',
+        madeFrom: 'lorum ipsum dolor and all that',
+        howToPlay: '',
         sound: sample(sounds.baritone),
         videoUrl: ''
       },
       {
         name: 'bass',
         icon: icons.bassIcon,
-        description: 'lorum ipsum dolor and all that',
+        madeFrom: 'lorum ipsum dolor and all that',
+        howToPlay: '',
         sound: sample(sounds.bass),
         videoUrl: ''
       }
