c7b666c52ee5ef9f1af1daa675d161da36757da5
Adam Thometz <adamthometz@gmail.com>
2022-05-04 14:22:57 -0400

    recentered remove button

--- src/score-keeper/ScoreKeeper.test.js
+++ src/score-keeper/ScoreKeeper.test.js
@@ -2,9 +2,10 @@ import React from "react";
 import renderWithProvider from "../_testUtils/renderWithProvider";
 // import { screen } from "@testing-library/react";
 import ScoreKeeper from "./ScoreKeeper";
+jest.mock('tone');
 
 describe('Score Keeper', () => {
   it('renders without crashing', () => {
     renderWithProvider(<ScoreKeeper />);
-  })
+  });
 })
