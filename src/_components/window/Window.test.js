f949aa6232a21f062cd3733508952fb0cdeb7ebe
Adam Thometz <adamthometz@gmail.com>
2022-05-05 14:25:10 -0400

    updated snapshot for window and window nav

--- src/_components/window/Window.test.js
+++ src/_components/window/Window.test.js
@@ -1,13 +1,13 @@
 import React from "react";
 import renderWithProvider from '../../_testUtils/renderWithProvider';
-import Window from './Window'
+import Window from './Window';
 
 describe('Window component', () => {
   it('renders without crashing', () => {
     renderWithProvider(<Window />);
   })
 
-  it('matches the snapshot of the button', () => {
+  it('matches the snapshot of the window', () => {
     const { asFragment } = renderWithProvider(<Window />);
     expect(asFragment()).toMatchSnapshot();
   })
