394f08d387502bc2163b8bcd738545ccaaa981ce
Adam Thometz <adamthometz@gmail.com>
2022-05-05 15:40:11 -0400

    reconfigured jest

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  // setupFiles: ['<rootDir>/src/setupTests.js'],
   moduleNameMapper: {
     '\\.(css)$': '<rootDir>/src/_testUtils/mocks/styleMock.js',
     '\\.(mp3|png|jpeg)$': '<rootDir>/src/_testUtils/mocks/fileMock.js'
