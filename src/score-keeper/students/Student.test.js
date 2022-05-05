c6999333ee68c85c4e425d25acce8adb73845701
Adam Thometz <adamthometz@gmail.com>
2022-05-05 18:34:07 -0400

    added test for student component

---
+++ src/score-keeper/students/Student.test.js
@@ -0,0 +1,14 @@
+import React from "react";
+import renderWithProvider from '../../_testUtils/renderWithProvider';
+import Student from './Student'
+
+describe('Student component', () => {
+  it('renders without crashing', () => {
+    renderWithProvider(<Student />);
+  })
+
+  it('matches the snapshot', () => {
+    const { asFragment } = renderWithProvider(<Student name='Eddie' color='#000000' points={0} />);
+    expect(asFragment()).toMatchSnapshot();
+  })
+})
