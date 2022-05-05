fe83be40e36774957fbf939b87e88b6ce3b8b4fd
Adam Thometz <adamthometz@gmail.com>
2022-05-05 14:26:45 -0400

    updated snapshot tests and minor code formatting

--- src/menu-landing/LandingPage.js
+++ src/menu-landing/LandingPage.js
@@ -6,12 +6,13 @@ import Welcome from "./welcome/Welcome";
 import Menu from "./menu/Menu";
 
 const LandingPage = () => {
-  const [hasVisited, setHasVisited] = useLocalStorage('visited')
- 
+  const [hasVisited, setHasVisited] = useLocalStorage('visited');
+
   return !hasVisited ? 
     <Window small page='WELCOME TO ART BUDDY!'>
       <Welcome setHasVisited={setHasVisited} />
-    </Window> :
+    </Window>
+    :
     <Window small>
       <Menu category='games' />
     </Window>
