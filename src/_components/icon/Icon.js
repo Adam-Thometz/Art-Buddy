53fcebb5d386d0b0c20b5e6c5b0483bf8d71953d
Adam Thometz <adamthometz@gmail.com>
2022-05-05 07:52:27 -0400

    refactored to use id instead of class for selection

--- src/_components/icon/Icon.js
+++ src/_components/icon/Icon.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Icon = ({ icon, text, onClick = null, size = "250px", additionalClass = null }) => {
+const Icon = ({ icon, text, onClick = null, size = "250px", id = null }) => {
   const styles = {
     width: size,
     height: size,
@@ -8,7 +8,7 @@ const Icon = ({ icon, text, onClick = null, size = "250px", additionalClass = nu
   };
 
   return (
-    <div className={`Icon${additionalClass ? ` ${additionalClass}` : ''}`} onClick={onClick}>
+    <div className="Icon" onClick={onClick} id={id ? id : null}>
       <img style={styles} src={icon} alt="" />
       <p>{text}</p>
     </div>
