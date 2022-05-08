ec7593296e57ac52be1b5de2afb7ac36b162cffa
Adam Thometz <adamthometz@gmail.com>
2022-05-08 10:35:44 -0400

    added id to dropdown

--- src/_components/dropdown/Dropdown.js
+++ src/_components/dropdown/Dropdown.js
@@ -4,7 +4,7 @@ import './Dropdown.css';
 
 import downArrowIcon from './_icons/down-arrow.png';
 
-const Dropdown = ({ labelText, onClick, options, borderColor }) => {
+const Dropdown = ({ id, labelText, onClick, options, borderColor }) => {
   const [displayLabelText, setDisplayLabelText] = useState(labelText)
   const handleChangeLabel = e => {
     setDisplayLabelText(e.target.textContent);
@@ -13,7 +13,7 @@ const Dropdown = ({ labelText, onClick, options, borderColor }) => {
   return (
     <div className="Dropdown">
       <span className='Dropdown-label' style={{borderColor}}>{displayLabelText} {downArrow}</span>
-      <div className='Dropdown-options' onClick={onClick}>
+      <div className='Dropdown-options' onClick={onClick} id={id ? id : null}>
         {Object.keys(options).map((value, i) => (
           <p key={i} className='Dropdown-option' onClick={handleChangeLabel} id={value}>{options[value]}</p>
         ))}
