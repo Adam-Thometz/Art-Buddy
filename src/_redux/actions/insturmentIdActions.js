d572d8095791c66be0ab33d2affcd7ba6e76b215
Adam Thometz <adamthometz@gmail.com>
2022-05-09 07:52:03 -0400

    added functionality to the Listening Skills game

--- src/_redux/actions/insturmentIdActions.js
+++ src/_redux/actions/insturmentIdActions.js
@@ -1,4 +1,6 @@
 import { createAction } from "@reduxjs/toolkit";
 
+export const selectChoice = createAction('selectChoice');
 export const createSound = createAction('createSound');
+export const generateAnswer = createAction('generateAnswer');
 export const playSound = createAction('playSound');
--- src/_redux/reducers/instrumentIdReducer.js
+++ src/_redux/reducers/instrumentIdReducer.js
@@ -1,25 +1,52 @@
-import { createSound, playSound } from "../actions/insturmentIdActions";
+import { createSound, generateAnswer, playSound, selectChoice } from "../actions/insturmentIdActions";
 import { createReducer } from "@reduxjs/toolkit";
-import * as storedSounds from '../../instrument-id/_sounds/soundImports';
 
-import sample from "../../_utils/sample";
+import learnInstrumentOptions from '../../instrument-id/learnInstrumentOptions';
 
 const INITIAL_STATE = {
-  currInstrument: null
+  choice1: null,
+  choice2: null,
+  answer: null
 }
 
 const instrumentIdReducer = createReducer(INITIAL_STATE, (builder) => {
   builder
+    .addCase(selectChoice, (state, action) => {
+      const { id, level, choice } = action.payload;
+      const choices = learnInstrumentOptions[choice].instruments;
+      if (level === '1') {
+        if (id === '1') {
+          const instrument1Idx = Math.floor(Math.random() * choices.length);
+          state.choice1 = choices[instrument1Idx];
+        } else if (id === '2') {
+          const instrument2Idx = Math.floor(Math.random() * choices.length);
+          state.choice2 = choices[instrument2Idx];
+        }
+      } else {
+        const instrument1Idx = Math.floor(Math.random() * choices.length);
+        state.choice1 = choices[instrument1Idx];
+        let instrument2Idx = instrument1Idx;
+        while (instrument2Idx === instrument1Idx) {
+          instrument2Idx = Math.floor(Math.random() * choices.length);
+        }
+        state.choice2 = choices[instrument2Idx];
+      }
+    })
+    .addCase(generateAnswer, (state, action) => {
+      const { choice1, choice2 } = action.payload;
+      const answer = Math.ceil(Math.random() * 2);
+      // debugger;
+      state.answer = answer === 1 ? choice1 : choice2;
+    })
     .addCase(createSound, (state, action) => {
-      const instrument = action.payload;
-      const sound = storedSounds[instrument];
+      // const instrument = action.payload;
+      // const sound = storedSounds[instrument];
     })
     .addCase(playSound, (state, action) => {
-      debugger;
       const sound = action.payload;
       const audio = new Audio(sound);
       audio.play();
     })
 });
 
-export default instrumentIdReducer
+export default instrumentIdReducer;
