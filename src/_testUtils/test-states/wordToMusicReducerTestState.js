994f5f13f5052a9143b41c41b0865207c14d7800
Adam Thometz <adamthometz@gmail.com>
2022-05-10 15:53:04 -0400

    updated test states for reflect refactor

--- src/_testUtils/test-states/wordToMusicReducerTestState.js
+++ src/_testUtils/test-states/wordToMusicReducerTestState.js
@@ -1,52 +1,66 @@
 const testWordDisplayOneWord = [
-  [
-    {
-      letter: 'H',
-      note: null
-    },
-    {
-      letter: 'I',
-      note: null
-    }
-  ]
+  {
+    scale: 0,
+    word: [
+      {
+        letter: 'H',
+        note: null
+      },
+      {
+        letter: 'I',
+        note: null
+      }
+    ]
+  }
 ];
 
 const testWordDisplayTwoWords = [
-  [
-    {
-      letter: 'H',
-      note: null
-    },
-    {
-      letter: 'I',
-      note: null
-    }
-  ],
-  [
-    {
-      letter: 'H',
-      note: null
-    },
-    {
-      letter: 'E',
-      note: null
-    },
-    {
-      letter: 'L',
-      note: null
-    },
-    {
-      letter: 'L',
-      note: null
-    },
-    {
-      letter: 'O',
-      note: null
-    }
-  ]
+  {
+    scale: 0,
+    word: [
+      {
+        letter: 'H',
+        note: null
+      },
+      {
+        letter: 'I',
+        note: null
+      }
+    ]
+  },
+  {
+    scale: 0,
+    word: [
+      {
+        letter: 'H',
+        note: null
+      },
+      {
+        letter: 'E',
+        note: null
+      },
+      {
+        letter: 'L',
+        note: null
+      },
+      {
+        letter: 'L',
+        note: null
+      },
+      {
+        letter: 'O',
+        note: null
+      }
+    ]
+  }
 ];
+
+const testState = {
+  wordDisplay: testWordDisplayOneWord,
+};
 
 export {
   testWordDisplayOneWord,
-  testWordDisplayTwoWords
+  testWordDisplayTwoWords,
+  testState
 };
