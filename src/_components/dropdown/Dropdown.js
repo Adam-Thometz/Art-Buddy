aae244040d1cc9a887840fd4b14fd8226e840f02
Adam Thometz <adamthometz@gmail.com>
2022-05-10 15:52:37 -0400

    added a small setting for dropdown

--- src/_components/dropdown/Dropdown.js
+++ src/_components/dropdown/Dropdown.js
@@ -4,15 +4,19 @@ import './Dropdown.css';
 
 import downArrowIcon from './_icons/down-arrow.png';
 
-const Dropdown = ({ id, labelText, onClick, options, borderColor }) => {
+const Dropdown = ({ id, labelText, onClick, options, borderColor, small = false }) => {
   const [displayLabelText, setDisplayLabelText] = useState(labelText)
   const handleChangeLabel = e => {
     setDisplayLabelText(e.target.textContent);
   };
-  const downArrow = <img src={downArrowIcon} alt='' />
+  const smallStyles = small ? { fontSize: '0.75rem' } : null;
+  const smallStylesLabel = small ? { padding: '5px', border: '2px solid' } : null;
+  const smallStylesArrow = small ? { height: '8px', width: '12px' } : null
+
+  const downArrow = <img style={smallStylesArrow} src={downArrowIcon} alt='' />
   return (
-    <div className="Dropdown">
-      <span className='Dropdown-label' style={{borderColor}}>{displayLabelText} {downArrow}</span>
+    <div style={smallStyles} className="Dropdown">
+      <span className='Dropdown-label' style={{borderColor, ...smallStylesLabel}}>{displayLabelText} {downArrow}</span>
       <div className='Dropdown-options' onClick={onClick} id={id ? id : null}>
         {Object.keys(options).map((value, i) => (
           <p key={i} className='Dropdown-option' onClick={handleChangeLabel} id={value}>{options[value]}</p>
