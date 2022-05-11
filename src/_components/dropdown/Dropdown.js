0a4f0a25c64fb02ff89240b9b96fca6846773d79
Adam Thometz <adamthometz@gmail.com>
2022-05-11 09:20:53 -0400

    refactored small styles

--- src/_components/dropdown/Dropdown.js
+++ src/_components/dropdown/Dropdown.js
@@ -9,15 +9,19 @@ const Dropdown = ({ id, labelText, onClick, options, borderColor, small = false 
   const handleChangeLabel = e => {
     setDisplayLabelText(e.target.textContent);
   };
-  const smallStyles = small ? { fontSize: '0.75rem' } : null;
-  const smallStylesLabel = small ? { padding: '5px', border: '2px solid' } : null;
-  const smallStylesArrow = small ? { height: '8px', width: '12px' } : null
 
-  const downArrow = <img style={smallStylesArrow} src={downArrowIcon} alt='' />
+  const smallStyles = small ? {
+    general: { fontSize: '0.75rem' },
+    label: { padding: '5px', border: '2px solid' },
+    arrow: { height: '8px', width: '12px' },
+    option: { fontSize: '0.75rem' },
+  } : {};
+
+  const downArrow = <img style={smallStyles.arrow} src={downArrowIcon} alt='' />
   return (
-    <div style={smallStyles} className="Dropdown">
-      <span className='Dropdown-label' style={{borderColor, ...smallStylesLabel}}>{displayLabelText} {downArrow}</span>
-      <div className='Dropdown-options' onClick={onClick} id={id ? id : null}>
+    <div style={smallStyles.general} className="Dropdown">
+      <span className='Dropdown-label' style={{borderColor, ...smallStyles.label}}>{displayLabelText} {downArrow}</span>
+      <div className='Dropdown-options' style={smallStyles.option} onClick={onClick} id={id ? id : null}>
         {Object.keys(options).map((value, i) => (
           <p key={i} className='Dropdown-option' onClick={handleChangeLabel} id={value}>{options[value]}</p>
         ))}
