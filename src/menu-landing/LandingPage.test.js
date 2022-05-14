3b7dc7e97510e93186c4394762138ec1eeb10e7b
Adam Thometz <adamthometz@gmail.com>
2022-05-13 23:25:58 -0400

    added test for use of localStorage

--- src/menu-landing/LandingPage.test.js
+++ src/menu-landing/LandingPage.test.js
@@ -1,14 +1,23 @@
 import React from "react";
 import renderWithProvider from '../_testUtils/renderWithProvider';
+import { screen } from "@testing-library/react";
 import LandingPage from './LandingPage'
+import LocalStorageMock from '../_testUtils/mocks/localStorageMock';
 
 describe('LandingPage component', () => {
   it('renders without crashing', () => {
     renderWithProvider(<LandingPage />);
-  })
+  });
 
-  it('matches the snapshot of the landing page displaying the welcome page', () => {
+  it('matches the snapshot of the landing page displaying the games page', () => {
     const { asFragment } = renderWithProvider(<LandingPage />);
     expect(asFragment()).toMatchSnapshot();
-  })
-})
+  });
+
+  it('renders welcome page only when visited is NOT set to true', () => {
+    const localStorage = new LocalStorageMock();
+    localStorage.setItem('visited', false)
+    renderWithProvider(<LandingPage />);
+    expect(screen.getByText('WELCOME TO ART BUDDY!')).toBeInTheDocument();
+  });
+});
--- src/menu-landing/welcome/Welcome.test.js
+++ src/menu-landing/welcome/Welcome.test.js
@@ -5,10 +5,10 @@ import Welcome from './Welcome'
 describe('Welcome component', () => {
   it('renders without crashing', () => {
     renderWithProvider(<Welcome />);
-  })
+  });
 
   it('matches the snapshot of the welcome page', () => {
     const { asFragment } = renderWithProvider(<Welcome />);
     expect(asFragment()).toMatchSnapshot();
-  })
-})
+  });
+});
