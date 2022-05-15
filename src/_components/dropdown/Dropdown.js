d45a015e214d9efc2f100f6afa48e565edfef0af
Adam Thometz <adamthometz@gmail.com>
2022-05-15 15:00:37 -0400

    refactored styling to eliminate unnecessary css styles

--- src/_components/dropdown/Dropdown.js
+++ src/_components/dropdown/Dropdown.js
@@ -4,24 +4,29 @@ import './Dropdown.css';
 
 import downArrowIcon from './_icons/down-arrow.png';
 
-const Dropdown = ({ id, labelText, onClick, options, borderColor, small = false }) => {
+const Dropdown = ({ id = null, labelText, onClick, options, borderColor, small = false }) => {
   const [displayLabelText, setDisplayLabelText] = useState(labelText)
   const handleChangeLabel = e => {
     setDisplayLabelText(e.target.textContent);
   };
 
-  const smallStyles = small ? {
+  const styles = small ? {
     general: { fontSize: '0.75rem' },
     label: { padding: '5px', border: '2px solid' },
     arrow: { height: '8px', width: '12px' },
-    option: { fontSize: '0.75rem' },
-  } : {};
+    options: { fontSize: '0.75rem' },
+  } : {
+    general: {  fontSize: '1.5rem' },
+    label: { padding: '10px', border: '5px solid' },
+    arrow: { height: '16px', width: '24px' },
+    options: { fontSize: '1rem' }
+  };
 
-  const downArrow = <img style={smallStyles.arrow} src={downArrowIcon} alt='' />
+  const downArrow = <img style={styles.arrow} src={downArrowIcon} alt='' />
   return (
-    <div style={smallStyles.general} className="Dropdown">
-      <span className='Dropdown-label' style={{borderColor, ...smallStyles.label}}>{displayLabelText} {downArrow}</span>
-      <div className='Dropdown-options' style={smallStyles.option} onClick={onClick} id={id ? id : null}>
+    <div style={styles.general} className="Dropdown">
+      <span className='Dropdown-label' style={{borderColor, ...styles.label}}>{displayLabelText} {downArrow}</span>
+      <div className='Dropdown-options' style={styles.options} onClick={onClick} id={id !== null ? id : null}>
         {Object.keys(options).map((value, i) => (
           <p key={i} className='Dropdown-option' onClick={handleChangeLabel} id={value}>{options[value]}</p>
         ))}
