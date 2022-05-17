05f08356c0db52d1c3eedd82912fe8092595a6ea
Adam Thometz <adamthometz@gmail.com>
2022-05-16 22:04:36 -0400

    added playMelody functionality

--- src/instrument-id/play/song-maker/SongMaker.js
+++ src/instrument-id/play/song-maker/SongMaker.js
@@ -1,21 +1,24 @@
 import React from 'react';
 
 import { useSelector, useDispatch } from 'react-redux';
-import { addInstrument, selectInstrument } from '../../../_redux/actions/insturmentIdActions';
+import { addInstrument, selectInstrument, selectMelody } from '../../../_redux/actions/insturmentIdActions';
 
 import './SongMaker.css';
 
 import Button from '../../../_components/button/Button';
 import Dropdown from '../../../_components/dropdown/Dropdown';
 import Icon from '../../../_components/icon/Icon';
-
-import { addInstrumentIcon } from '../../_icons/iconImports';
-import { instrumentOptions, melodyOptions } from './dropdownOptions';
 import Popup from '../../../_components/popup/Popup';
 import SaveSong from './save-song/SaveSong';
 
+import { addInstrumentIcon } from '../../_icons/iconImports';
+import { instrumentOptions, melodyOptions } from './dropdownOptions';
+import { Buffer } from 'tone';
+import * as sounds from '../../_sounds/soundImports';
+import { isRhythmicInstrument, playMelody } from '../../_utils/play';
+
 const SongMaker = () => {
-  const instruments = useSelector(state => state.instrumentId.instruments);
+  const instrumentsToPlay = useSelector(state => state.instrumentId.instruments);
   const dispatch = useDispatch()
   
   const handleAddInstrument = e => {
@@ -24,9 +27,32 @@ const SongMaker = () => {
   }
   
   const handleSelectInstrument = e => {
+    const id = +e.currentTarget.id;
+    const instrumentId = e.target.id;
+    const instrumentName = e.target.innerText;
+    dispatch(selectInstrument({ id, instrumentId, instrumentName }));
+    window[`buffer${id+1}`] = new Buffer(sounds[instrumentId]);
+  }
+
+  const handleSelectMelody = e => {
     const id = e.currentTarget.id;
-    const instrument = e.target.id
-    dispatch(selectInstrument({ id, instrument }));
+    const melodyId = e.target.id;
+    dispatch(selectMelody({ id, melodyId }));
+  };
+
+  const playInstruments = () => {
+    for (let i = 0; i < instrumentsToPlay.length; i++) {
+      const instrument = instrumentsToPlay[i];
+      if (!instrument) continue;
+
+      const buffer = window[`buffer${i+1}`]
+      const { instrumentName, melody } = instrument;
+      if (isRhythmicInstrument(instrumentName)) {
+
+      } else {
+        playMelody(melody, buffer);
+      }
+    }
   }
 
   const savePopup = (
@@ -35,9 +61,9 @@ const SongMaker = () => {
       trigger={<Button colorId={3}>SAVE</Button>}
       popup={<SaveSong />}
     />
-  )
+  );
 
-  const instrumentDisplay = instruments.map((instrument, i) => (
+  const instrumentDisplay = instrumentsToPlay.map((instrument, i) => (
     !instrument ? (
       <Icon
         icon={addInstrumentIcon}
@@ -54,11 +80,12 @@ const SongMaker = () => {
           options={instrumentOptions}
           onClick={handleSelectInstrument}
         />
-        {instrument.instrument ? (
+        {instrument.instrumentId ? (
           <Dropdown
             id={i}
             labelText='MELODY'
             options={melodyOptions}
+            onClick={handleSelectMelody}
           />
         ) : null}
       </div>
@@ -71,7 +98,7 @@ const SongMaker = () => {
       <hr/>
       <div className='SongMaker-button-instrument-wrapper'>
         <div className='SongMaker-buttons'>
-          <Button colorId={0}>PLAY</Button>
+          <Button colorId={0} onClick={playInstruments}>PLAY</Button>
           {savePopup}
         </div>
         <div className='SongMaker-instruments'>
