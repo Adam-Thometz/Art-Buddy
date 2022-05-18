43e9a7a63c72405c23fe2e81ce50dba4ba23e243
Adam Thometz <adamthometz@gmail.com>
2022-05-18 10:47:23 -0400

    changed maxScore to 4

--- src/instrument-id/play/report-card/ReportCard.js
+++ src/instrument-id/play/report-card/ReportCard.js
@@ -5,28 +5,25 @@ import { useSelector } from 'react-redux';
 import './ReportCard.css';
 
 import { emptyFirst, emptyMid, emptyLast, fullFirst, fullMid, fullLast } from './_icons/iconImports';
-import learnInstrumentOptions from '../../learnInstrumentOptions';
 
 const ReportCard = ({ level }) => {
   const reportCard = useSelector(state => state.instrumentId[`reportCard${level}`]);
+  
   const scoreDisplay = Object.keys(reportCard).map(family => {
     const score = reportCard[family].length;
-    const maxScore = learnInstrumentOptions[family].instruments.length;
-    const midRange = Array.from({length: maxScore-2}, (x, i) => i+1);
-    const midScoreDisplay = midRange.map(s => (
-      <img src={score >= s+1 ? fullMid : emptyMid} alt='' />
-    ));
+    const maxScore = 4;
     return (
       <div className='ReportCard-score-display'>
         <span>{family.toUpperCase()} FAMILY</span>
         <div className='ReportCard-visual-score'>
           <img src={score >= 1 ? fullFirst : emptyFirst} alt='' />
-          {midScoreDisplay}
+          <img src={score >= 2 ? fullMid : emptyMid} alt='' />
+          <img src={score >= 3 ? fullMid : emptyMid} alt='' />
           <img src={score === maxScore ? fullLast : emptyLast} alt='' />
         </div>
         <span className='ReportCard-text-score'>{score}/{maxScore} passed</span>
       </div>
-    )
+    );
   });
 
   return (
