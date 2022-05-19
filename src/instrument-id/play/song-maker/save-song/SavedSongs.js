36e41b5ca90f2b1d7f2bbc3a2c19930f5d155aff
Adam Thometz <adamthometz@gmail.com>
2022-05-19 13:14:32 -0400

    added delete saved song feature

--- src/instrument-id/play/song-maker/save-song/SavedSongs.js
+++ src/instrument-id/play/song-maker/save-song/SavedSongs.js
@@ -6,14 +6,41 @@ import './SavedSongs.css';
 import Icon from '../../../../_components/icon/Icon';
 
 import { smallPlayIcon, deleteIcon } from '../../../_icons/iconImports';
+import { createBuffers, getBuffers } from '../../../_utils/buffers';
+import { play } from '../../../_utils/play';
 
 const SavedSongs = () => {
-  const [savedSongs] = useLocalStorage('instrument-id-saved-songs');
+  const [savedSongs, setSavedSongs] = useLocalStorage('instrument-id-saved-songs');
   const [selectedSong, setSelectedSong] = useState(null);
   const songs = new Map(JSON.parse(savedSongs));
 
   const handleSelect = e => {
-    setSelectedSong(e.target.innerText);
+    const songName = e.target.innerText;
+    setSelectedSong(songName);
+    const song = songs.get(songName);
+    for (let instrument of song) {
+      if (!instrument) continue;
+      createBuffers(instrument.instrumentId);
+    }
+  }
+
+  const handlePlay = () => {
+    const song = songs.get(selectedSong);
+    for (let instrument of song) {
+      if (!instrument) continue;
+
+      const { instrumentId, melodyId, isRhythm } = instrument;
+      if (!melodyId) continue;
+      
+      const { buffers } = getBuffers(instrumentId);
+      play({ melodyId, buffers, isRhythm });
+    }
+  }
+
+  const handleDelete = () => {
+    if (!selectedSong) return;
+    songs.delete(selectedSong);
+    setSavedSongs(JSON.stringify(Array.from(songs.entries())));
   }
 
   const songDisplay = Array.from(songs.keys()).map((key) => (
@@ -23,8 +50,8 @@ const SavedSongs = () => {
   return (
     <div className='SavedSongs'>
       <div className='SavedSongs-options'>
-        <Icon icon={smallPlayIcon} text='Play' size='49px' />
-        <Icon icon={deleteIcon} text='Delete' size='49px' />
+        <Icon icon={smallPlayIcon} text='Play' size='49px' onClick={handlePlay} />
+        <Icon icon={deleteIcon} text='Delete' size='49px' onClick={handleDelete} />
       </div>
       <div className='SavedSongs-song-list'>
         {songDisplay}
