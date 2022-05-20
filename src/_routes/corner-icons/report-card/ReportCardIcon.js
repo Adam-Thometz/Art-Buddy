1ef386226b763085099f0b0e0cf10c81b89526f3
Adam Thometz <adamthometz@gmail.com>
2022-05-20 13:16:23 -0400

    styling

--- src/_routes/corner-icons/report-card/ReportCardIcon.js
+++ src/_routes/corner-icons/report-card/ReportCardIcon.js
@@ -9,7 +9,7 @@ import ReportCard from '../../../instrument-id/play/report-card/ReportCard';
 
 const ReportCardIcon = () => {
   const { level } = useParams()
-  const popupTrigger = (<Icon icon={reportCardIcon} text='REPORT CARD' size='50px' />)
+  const popupTrigger = (<Icon icon={reportCardIcon} text='REPORT CARD' size='50px' width='34px' />)
   return (
     <Popup
       title='REPORT CARD'
