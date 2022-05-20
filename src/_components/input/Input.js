d1cb0203b7add448f31ac5801e8331924c8c27ab
Adam Thometz <adamthometz@gmail.com>
2022-05-20 11:30:23 -0400

    added keyboard icon

--- src/_components/input/Input.js
+++ src/_components/input/Input.js
@@ -2,12 +2,16 @@ import React, { useState } from 'react';
 
 import './Input.css';
 
+import Icon from '../icon/Icon';
+
+import keyboard from './keyboard.png';
+
 const Input = ({ type = 'text', label, id,  }) => {
   const [input, setInput] = useState('');
 
   const handleChange = e => {
-    setInput(e.target.value)
-  }
+    setInput(e.target.value);
+  };
 
   return (
     <div className='Input'>
@@ -20,6 +24,9 @@ const Input = ({ type = 'text', label, id,  }) => {
         onChange={handleChange}
         value={input}
       />
+      <div className='Input-keyboard'>
+        <Icon icon={keyboard} text='OPEN KEYBOARD' size='40px' />
+      </div>
     </div>
   );
 };
