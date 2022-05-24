66f1e77354364b6b99909126222a4eebe77ef61f
Adam Thometz <adamthometz@gmail.com>
2022-05-24 18:40:49 -0400

    added a separate setup functions folder for tests

---
+++ src/_testUtils/setup-functions/ListeningSkillsTestSetup.js
@@ -0,0 +1,12 @@
+import { screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+export function setupChoices() {
+  Math.random = jest.fn();
+  Math.random.mockReturnValueOnce(0.25).mockReturnValue(0.5).mockReturnValueOnce(0.5);
+
+  const dropdown = screen.getByText('CHOOSE FAMILY');
+  userEvent.hover(dropdown);
+  const option = screen.queryAllByText('BRASS');
+  userEvent.click(option[0]);
+}
---
+++ src/_testUtils/setup-functions/instrumentIdReducerTestSetup.js
@@ -0,0 +1,22 @@
+import instrumentIdReducer from "../../_redux/reducers/instrumentIdReducer";
+import { selectChoice, selectInstrument } from "../../_redux/actions/insturmentIdActions";
+
+export function setupChoices() {
+  Math.random = jest.fn();
+  Math.random.mockReturnValueOnce(0.2).mockReturnValue(0.8);
+  const result = instrumentIdReducer(undefined, selectChoice({
+    id: 1,
+    level: '2',
+    choice: 'electronic'
+  }));
+  return result;
+};
+
+export function setupInstrument({ id, isRhythm, withMelody = undefined }) {
+  const instrumentId = isRhythm ? 'drumSet' : 'acousticGuitar'
+  const result = instrumentIdReducer(withMelody, selectInstrument({
+    id,
+    instrumentId
+  }));
+  return result;
+};
