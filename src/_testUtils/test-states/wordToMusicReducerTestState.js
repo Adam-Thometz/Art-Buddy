393e41abbdc47c80c68ef3fe4adaea34419134bb
Adam Thometz <adamthometz@gmail.com>
2022-05-24 11:52:50 -0400

    updated wordToMusic test state

--- src/_testUtils/test-states/wordToMusicReducerTestState.js
+++ src/_testUtils/test-states/wordToMusicReducerTestState.js
@@ -1,62 +1,54 @@
 const testWordDisplayOneWord = [
-  {
-    scale: 0,
-    word: [
-      {
-        letter: 'H',
-        note: null
-      },
-      {
-        letter: 'I',
-        note: null
-      }
-    ]
-  }
+  [
+    {
+      letter: 'H',
+      note: null
+    },
+    {
+      letter: 'I',
+      note: null
+    }
+  ]
 ];
 
 const testWordDisplayTwoWords = [
-  {
-    scale: 0,
-    word: [
-      {
-        letter: 'H',
-        note: null
-      },
-      {
-        letter: 'I',
-        note: null
-      }
-    ]
-  },
-  {
-    scale: 0,
-    word: [
-      {
-        letter: 'H',
-        note: null
-      },
-      {
-        letter: 'E',
-        note: null
-      },
-      {
-        letter: 'L',
-        note: null
-      },
-      {
-        letter: 'L',
-        note: null
-      },
-      {
-        letter: 'O',
-        note: null
-      }
-    ]
-  }
+  [
+    {
+      letter: 'H',
+      note: null
+    },
+    {
+      letter: 'I',
+      note: null
+    }
+  ],
+  [
+    {
+      letter: 'H',
+      note: null
+    },
+    {
+      letter: 'E',
+      note: null
+    },
+    {
+      letter: 'L',
+      note: null
+    },
+    {
+      letter: 'L',
+      note: null
+    },
+    {
+      letter: 'O',
+      note: null
+    }
+  ]
 ];
 
 const testState = {
   wordDisplay: testWordDisplayOneWord,
+  scale: 0
 };
 
 export {
