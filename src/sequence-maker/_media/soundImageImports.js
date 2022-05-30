6b3d7368a716adea64ae7de01ed077501dfcb328
Adam Thometz <adamthometz@gmail.com>
2022-05-30 15:54:23 -0400

    removed sound files and put them on a separate server

--- src/sequence-maker/_media/soundImageImports.js
+++ src/sequence-maker/_media/soundImageImports.js
@@ -1,23 +1,4 @@
 ////////////
-// SOUNDS //
-////////////
-
-// Body Sounds (continuous)
-import clappingContinuous from './sounds/body-sounds/clapping_continuous.mp3';
-import stompingContinuous from './sounds/body-sounds/stomping_continuous.mp3';
-import yeahContinuous from './sounds/body-sounds/yeah_continuous.mp3';
-
-// Body Sounds (once)
-import clappingOnce from './sounds/body-sounds/clapping_once.mp3';
-import stompingOnce from './sounds/body-sounds/stomping_once.mp3';
-import yeahOnce from './sounds/body-sounds/yeah_once.mp3';
-
-// Animals
-import bark from './sounds/animals/dog.mp3';
-import meow from './sounds/animals/cat.mp3';
-import tweet from './sounds/animals/bird.mp3';
-
-////////////
 // IMAGES //
 ////////////
 import stop from './images/stop.png';
@@ -31,6 +12,25 @@ import yeah from './images/body-sounds/yeah.png';
 import cat from './images/animals/cat.png';
 import dog from './images/animals/dog.png';
 import bird from './images/animals/bird.png';
+
+////////////
+// SOUNDS //
+////////////
+
+// Body Sounds (continuous)
+const clappingContinuous = 'https://download855.mediafire.com/c0w5zv5f60bg/39n20xrgrsvjqvd/clapping_continuous.mp3';
+const stompingContinuous = 'https://download1505.mediafire.com/erhm884mgskg/xyypua5w35pvj4o/stomping_continuous.mp3';
+const yeahContinuous = 'https://download1319.mediafire.com/jy95fm1tb4ng/kny7232s72twdsp/yeah_continuous.mp3';
+
+// Body Sounds (once)
+const clappingOnce = 'https://download1337.mediafire.com/g7oetdmjqk7g/2tzywfo1040nrsh/clapping_once.mp3';
+const stompingOnce = 'https://download856.mediafire.com/gml6ofv8jndg/1jclnr3wo4hmmah/stomping_once.mp3';
+const yeahOnce = 'https://download1346.mediafire.com/l3zsh08mbg7g/lggp0d17qiaf3j2/yeah_once.mp3';
+
+// Animals
+const bark = 'https://download1321.mediafire.com/1o4duv1gibag/ybmxh1p4jlad45k/dog.mp3';
+const meow = 'https://download1073.mediafire.com/xfkxvmeffc9g/0nwt3g1pabordxt/cat.mp3';
+const tweet = 'https://download1490.mediafire.com/vrtj3xigrqwg/mpf34upqr2yk2zl/bird.mp3';
 
 const sounds = {
   clappingContinuous,
