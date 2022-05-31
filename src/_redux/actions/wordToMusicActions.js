1da197bf7b6b32c238c4ff02294ddd533f20a4ab
Adam Thometz <adamthometz@gmail.com>
2022-05-31 09:14:18 -0400

    removed unused actions

--- src/_redux/actions/wordToMusicActions.js
+++ src/_redux/actions/wordToMusicActions.js
@@ -2,8 +2,6 @@ import { createAction } from "@reduxjs/toolkit";
 
 export const createWords = createAction('createWords');
 export const fillLetter = createAction('fillLetter');
-export const playNote = createAction('playNote');
-export const playMelody = createAction('playMelody');
 export const changeScale = createAction('changeScale');
 export const toggleUpperCase = createAction('toggleUpperCase');
 export const clearGame = createAction('clearGame');
