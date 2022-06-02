7c5f40a65b53c482808b083713a612e282ca4260
Adam Thometz <adamthometz@gmail.com>
2022-06-02 12:16:48 -0400

    added test for alphabet table

---
+++ src/music-decoder/alphabet-table/AlphabetTable.test.js
@@ -0,0 +1,14 @@
+import React from "react";
+import renderWithProvider from '../../_testUtils/renderWithProvider';
+import AlphabetTable from './AlphabetTable';
+
+describe('AlphabetTable component', () => {
+  it('renders without crashing', () => {
+    renderWithProvider(<AlphabetTable />);
+  });
+
+  it('matches snapshot', () => {
+    const { asFragment } = renderWithProvider(<AlphabetTable />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
