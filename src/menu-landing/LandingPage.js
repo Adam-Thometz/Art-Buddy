8af81a3564b3968b1123a990d23681c1ff0232cd
Adam Thometz <adamthometz@gmail.com>
2022-06-06 18:12:29 -0400

    updated snapshots

--- src/menu-landing/LandingPage.js
+++ src/menu-landing/LandingPage.js
@@ -1,21 +1,15 @@
 import React from "react";
 import useLocalStorage from "../_hooks/useLocalStorage";
 
-import Window from "../_components/window/Window";
 import Welcome from "./welcome/Welcome";
 import Menu from "./menu/Menu";
 
 const LandingPage = () => {
   const [hasVisited, setHasVisited] = useLocalStorage('visited');
 
-  return !hasVisited ? 
-    <Window small>
-      <Welcome setHasVisited={setHasVisited} />
-    </Window>
-    :
-    <Window small>
-      <Menu category='games' />
-    </Window>
+  return !hasVisited ?
+    <Welcome setHasVisited={setHasVisited} /> :
+    <Menu category='games' />
 };
 
 export default LandingPage;
