bc91ed106efec27619c8640f4d3c6a333f322728
Adam Thometz <adamthometz@gmail.com>
2022-06-08 17:24:51 -0400

    added toggleNote function

--- src/_redux/actions/wordToMusicActions.js
+++ src/_redux/actions/wordToMusicActions.js
@@ -4,5 +4,6 @@ export const createWords = createAction('createWords');
 export const fillLetter = createAction('fillLetter');
 export const changeScale = createAction('changeScale');
 export const changeSound = createAction('changeSound');
+export const toggleNote = createAction('toggleNote');
 export const toggleUpperCase = createAction('toggleUpperCase');
 export const clearGame = createAction('clearGame');
