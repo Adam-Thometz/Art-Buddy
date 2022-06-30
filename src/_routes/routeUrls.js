50529cd7a2f10e4c65962c251a6a456222b6ced3
Adam Thometz <adamthometz@gmail.com>
2022-06-30 11:34:56 -0400

    refactored route urls

--- src/_routes/routeUrls.js
+++ src/_routes/routeUrls.js
@@ -3,20 +3,21 @@ const urls = {
   artGames: '/art',
   allGames: '/all',
   byIEPGoals: '/goals',
-  wordToMusicUrl: '/word-to-music',
-  sequencerUrl: '/sequencer',
-  instrumentIdUrl: '/instrument-id',
-  scoreKeeperUrl: '/score-keeper'
+  wordToMusic: '/word-to-music',
+  sequenceMaker: '/sequence-maker',
+  jumpIntoRhythm: '/jump-into-rhythm',
+  instrumentId: '/instrument-id',
+  scoreKeeper: '/score-keeper'
 }
 
 export const instrumentIdUrls = {
-  learnUrl: '/instrument-id/learn',
-  learnFamilyUrl: '/instrument-id/learn/:family',
-  learnInstrumentUrl: '/instrument-id/learn/:family/:instrument',
-  playUrl: '/instrument-id/play',
-  playListeningUrl: '/instrument-id/play/listening-skills',
-  playListeningLevelUrl: '/instrument-id/play/listening-skills/:level',
-  playSongMakerUrl: '/instrument-id/play/song-maker',
+  learn: '/instrument-id/learn',
+  learnFamily: '/instrument-id/learn/:family',
+  learnInstrument: '/instrument-id/learn/:family/:instrument',
+  play: '/instrument-id/play',
+  playListening: '/instrument-id/play/listening-skills',
+  playListeningLevel: '/instrument-id/play/listening-skills/:level',
+  playSongMaker: '/instrument-id/play/song-maker',
 }
 
 export default urls;
