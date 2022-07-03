33a7f17e48481b424dcb24a1097c711b388c39c5
Adam Thometz <adamthometz@gmail.com>
2022-07-03 15:18:02 -0400

    renamed _navbar to navbar

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './App.css';
 
-import Navbar from './_navbar/Navbar';
+import Navbar from './navbar/Navbar';
 import ArtBuddyRoutes from './_routes/ArtBuddyRoutes';
 
 const App = () => {
