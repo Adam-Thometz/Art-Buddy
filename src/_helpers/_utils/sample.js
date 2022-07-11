4a9a1a4fe8bf98572e8e39fcb541fa9dc378e18f
Adam Thometz <adamthometz@gmail.com>
2022-07-11 17:33:13 -0400

    code formatting

--- src/_helpers/_utils/sample.js
+++ src/_helpers/_utils/sample.js
@@ -1,11 +1,11 @@
 import { Sampler, Volume } from "tone";
 
 /** sample
- * Purpose: takes a sound and a number for volume and returns a sample to play notes with. Used all over the app
+ * Purpose: takes a sound and a number for volume and returns a sample to play notes with. Used mainly in play-related function
  */
 
 const sample = (sound, volume) => {
-  const vol = new Volume(volume).toDestination()
+  const vol = new Volume(volume).toDestination();
   return new Sampler({
     urls: {
       C3: sound
