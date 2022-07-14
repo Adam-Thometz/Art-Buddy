d4b4fef839154f036ded6dedf21e64f923698699
Adam Thometz <adamthometz@gmail.com>
2022-07-14 11:52:58 -0400

    code formatting

--- src/_helpers/sequence-maker/createBuffer.js
+++ src/_helpers/sequence-maker/createBuffer.js
@@ -2,12 +2,12 @@ import soundInfo from "_data/sequence-maker/soundInfo";
 import { Buffer } from "tone";
 
 /** createBuffer
- * Purpose: creates a buffer to place in the window object to make a sample when needed
+ * Purpose: creates a buffer and places it in the window object to make a sample when needed
  * Found in: SoundOptions.js
  */
 
 export default function createBuffer({ category, soundId }) {
-  const sound = soundInfo[category][soundId].sound;
+  const { sound } = soundInfo[category][soundId];
   const buffer = new Buffer(sound);
   window[`${soundId}Buffer`] = buffer;
 };
