ae884afea4569592b4b37219736aa5b8b18a50c8
Adam Thometz <adamthometz@gmail.com>
2022-10-04 11:36:41 -0400

    updated tests

--- src/home/Home.test.js
+++ src/home/Home.test.js
@@ -8,6 +8,13 @@ describe('Home component', () => {
   });
 
   it('matches the snapshot of the landing page displaying the games page', () => {
+    window.localStorage.setItem('visited', true);
+    const { asFragment } = renderWithProvider(<Home />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+  
+  it('matches the snapshot of the landing page displaying the welcome page', () => {
+    window.localStorage.setItem('visited', false);
     const { asFragment } = renderWithProvider(<Home />);
     expect(asFragment()).toMatchSnapshot();
   });
