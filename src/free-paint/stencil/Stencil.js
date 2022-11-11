2ef2e8be6ea5d8a1f9cb51c37ef8b84fdc8faf93
Adam Thometz <adamthometz@gmail.com>
2022-11-11 17:24:54 -0500

    added clear feature

--- src/free-paint/stencil/Stencil.js
+++ src/free-paint/stencil/Stencil.js
@@ -1,16 +1,29 @@
 import React from 'react';
 
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import './Stencil.css';
 
+import close from '_media/general-icons/close.png';
+import { setDisplay } from '_redux/free-paint/freePaintActions';
+
 const Stencil = () => {
   const { display } = useSelector(state => state.freePaint);
+  const dispatch = useDispatch();
+
+  const handleClearDisplay = () => dispatch(setDisplay(null));
+
   return display ? 
     <div className='Stencil'>
-      
+      <img
+        className='Stencil-clear'
+        src={close}
+        alt=''
+        aria-label='Clear the stencil'
+        onClick={handleClearDisplay}
+      />
       {display.startsWith('data:image')
-        ? <img src={display} alt='' />
+        ? <img className='Stencil-shape' src={display} alt='' />
         : <span className='Stencil-text'>{display}</span>
       }
     </div> : null;
