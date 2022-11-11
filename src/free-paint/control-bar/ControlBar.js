f2cdeeae3bc5189aa0b45a901359424364da40b2
Adam Thometz <adamthometz@gmail.com>
2022-11-10 22:59:54 -0500

    cleanup

--- src/free-paint/control-bar/ControlBar.js
+++ src/free-paint/control-bar/ControlBar.js
@@ -36,31 +36,43 @@ const ControlBar = () => {
 
   return (
     <section className='ControlBar'>
-      {/* <ButtonWrapper id='uppercase' colorId={4}  /> */}
+      {/* Uppercase */}
       <div className='ControlBar-button-wrapper'>
         <Button id='upperCase' otherStyles={buttonStyle} colorId={4} onClick={handleSetShownOptions}>ABC</Button>
         {shownOptions === 'upperCase' ? <StencilOptions id='upperCase' /> : null}
       </div>
+      {/* Lowercase */}
       <div className='ControlBar-button-wrapper'>
         <Button id='lowerCase' otherStyles={buttonStyle} colorId={0} onClick={handleSetShownOptions}>abc</Button>
         {shownOptions === 'lowerCase' ? <StencilOptions id='lowerCase' /> : null}
       </div>
+      {/* Numbers */}
       <div className='ControlBar-button-wrapper'>
         <Button id='numbers' otherStyles={buttonStyle} colorId={1} onClick={handleSetShownOptions}>123</Button>
         {shownOptions === 'numbers' ? <StencilOptions id='numbers' /> : null}
       </div>
+      {/* Shapes */}
       <div className='ControlBar-button-wrapper'>
         <Button id='shapes' otherStyles={buttonStyle} colorId={2} onClick={handleSetShownOptions}>
           <img className='ControlBar-shape-button' src={square} alt="Shapes"/>
         </Button>
         {shownOptions === 'shapes' ? <StencilOptions id='shapes' /> : null}
       </div>
+      {/* Pencil */}
       <div className='ControlBar-button-wrapper'>
-        <Button id='colors' otherStyles={{ ...buttonStyle, borderColor: color, color }} onClick={handleSetShownOptions}>
-          <Icon id='colors' size='45px' text="Pencil" icon={pencil} otherImgStyles={pencilColor} onClick={handleSetShownOptions} />
+        <Button id='colors' otherStyles={{ ...buttonStyle, color, borderColor: color }} onClick={handleSetShownOptions}>
+          <Icon
+            id='colors'
+            size='45px'
+            text="Pencil"
+            icon={pencil}
+            otherImgStyles={pencilColor}
+            onClick={handleSetShownOptions}
+          />
         </Button>
         {shownOptions === 'colors' ? <ColorOptions /> : null}
       </div>
+      {/* Eraser */}
       <Button otherStyles={buttonStyle}>
         <Icon size='45px' text="Eraser" icon={eraser} />
       </Button>
