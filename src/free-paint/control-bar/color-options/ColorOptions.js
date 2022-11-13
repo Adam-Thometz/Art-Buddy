b145e1b506c88ed49ae5447aacc096bd438e6882
Adam Thometz <adamthometz@gmail.com>
2022-11-12 21:12:57 -0500

    added erasing

--- src/free-paint/control-bar/color-options/ColorOptions.js
+++ src/free-paint/control-bar/color-options/ColorOptions.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { useDispatch } from 'react-redux';
-import { setColor } from '_redux/free-paint/freePaintActions';
+import { setColor, setIsErasing } from '_redux/free-paint/freePaintActions';
 
 import './ColorOptions.css';
 
@@ -10,7 +10,10 @@ import paintColors from '_data/free-paint/colors';
 const ColorOptions = () => {
   const dispatch = useDispatch();
 
-  const handleChangeColor = e => dispatch(setColor(e.target.id));
+  const handleChangeColor = e => {
+    dispatch(setColor(e.target.id));
+    dispatch(setIsErasing(false));
+  };
 
   return (
     <div className='ColorOptions'>
