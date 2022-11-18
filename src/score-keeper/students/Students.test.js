73edc0175290aa749fd45fd34ab013902a89d660
Adam Thometz <adamthometz@gmail.com>
2022-11-17 21:16:41 -0500

    updated tests

--- src/score-keeper/students/Students.test.js
+++ src/score-keeper/students/Students.test.js
@@ -1,16 +1,22 @@
 import React from "react";
 
+import { default as store } from "_redux/rootReducer";
+import { loadStudents } from "_redux/score-keeper/scoreKeeperActions";
+import { demoClass } from "_redux/settings/mainSettingsReducer";
+
 import renderWithProvider from '_testUtils/renderWithProvider';
 
 import Students from './Students';
 
 describe('Students component', () => {
+  store.dispatch(loadStudents(demoClass.students));
+
   it('renders without crashing', () => {
-    renderWithProvider(<Students />);
+    renderWithProvider(<Students />, { store });
   });
 
   it('matches snapshot', () => {
-    const { asFragment } = renderWithProvider(<Students />);
+    const { asFragment } = renderWithProvider(<Students />, { store });
     expect(asFragment()).toMatchSnapshot();
   });
 });
