a7428e54adbb0c48d259eaf8c8ad64fe2715c24a
Adam Thometz <adamthometz@gmail.com>
2022-11-17 19:55:24 -0500

    renamed variables

--- src/_hooks/useReportCard.js
+++ src/_hooks/useReportCard.js
@@ -12,13 +12,13 @@ export const INITIAL_REPORT_CARD = {
 const useReportCard = (level) => {
   const key = `instrument-id-level-${level}-report-card`;
   const initialValue = localStorage.getItem(key) || JSON.stringify(INITIAL_REPORT_CARD);
-  const [state, setState] = useState(initialValue);
+  const [reportCard, setReportCard] = useState(initialValue);
 
   useEffect(() => {
-    localStorage.setItem(key, state);
-  }, [key, state]);
+    localStorage.setItem(key, reportCard);
+  }, [key, reportCard]);
 
-  return [JSON.parse(state), setState];
+  return [JSON.parse(reportCard), setReportCard];
 };
 
 export default useReportCard;
