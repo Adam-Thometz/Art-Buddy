58141bf629562ffe5772bc1135833db2ca7a3666
Adam Thometz <adamthometz@gmail.com>
2022-11-20 18:26:56 -0500

    added delete feature

--- src/jump-into-rhythm/notes/note-block/NoteBlock.js
+++ src/jump-into-rhythm/notes/note-block/NoteBlock.js
@@ -1,11 +1,23 @@
 import React from 'react';
 
+import { useDispatch } from 'react-redux';
+import { deleteFromRhythm } from '_redux/jump-into-rhythm/jumpIntoRhythmActions';
+
 import './NoteBlock.css';
 
-const NoteBlock = ({ borderColor, notes }) => {
+import deleteBtn from '_media/general-icons/close.png';
+
+const NoteBlock = ({ borderColor, note, id }) => {
+  const dispatch = useDispatch();
+
+  const handleDeleteNote = () => dispatch(deleteFromRhythm(id));
+
   return (
     <div className='NoteBlock' style={{ borderColor }}>
-      {notes ? <img src={notes.img} alt='' /> : null}
+      {note ? <>
+        <img src={deleteBtn} className='NoteBlock-delete' alt='Delete note' onClick={handleDeleteNote} />
+        <img src={note.img} className='NoteBlock-note' alt='' />
+      </> : null}
     </div>
   );
 };
