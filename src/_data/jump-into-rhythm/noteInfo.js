892dce51b85bbaf1787e0c96ec2df52d8c4056f2
Adam Thometz <adamthometz@gmail.com>
2022-11-19 21:55:10 -0500

    added notes

--- src/_data/jump-into-rhythm/noteInfo.js
+++ src/_data/jump-into-rhythm/noteInfo.js
@@ -1,4 +1,10 @@
-import { quarterNote, eighthNotes } from "_media/jump-into-rhythm/_icons/notes/noteImports";
+import {
+  quarterNote,
+  eighthNotes,
+  eighthNote,
+  quarterRest,
+  eighthRest
+} from "_media/jump-into-rhythm/_icons/notes/noteImports";
 
 const noteInfo = {
   quarterNote: {
@@ -12,6 +18,24 @@ const noteInfo = {
     text: 'Eighth Notes',
     img: eighthNotes,
     duration: ['8n', '8n']
+  },
+  eighthNote: {
+    id: 'eighthNotes',
+    text: 'Eighth Note',
+    img: eighthNote,
+    duration: ['8n']
+  },
+  quarterRest: {
+    id: 'quarterRest',
+    text: 'Quarter Rest',
+    img: quarterRest,
+    duration: ['4n']
+  },
+  eighthRest: {
+    id: 'eighthRest',
+    text: 'Eighth Rest',
+    img: eighthRest,
+    duration: ['8n']
   }
 };
 
