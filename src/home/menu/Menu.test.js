ed5c6bf23f6e032c6b26219fd1f899f26d3c1bc3
Adam Thometz <adamthometz@gmail.com>
2022-11-20 19:44:24 -0500

    added test for tool rendering

--- src/home/menu/Menu.test.js
+++ src/home/menu/Menu.test.js
@@ -1,7 +1,9 @@
 import React from "react";
 import renderWithProvider from '_testUtils/renderWithProvider';
 import { screen } from "@testing-library/react";
+import { default as store } from "_redux/rootReducer";
 import Menu from "./Menu";
+import { changeMenu } from "_redux/settings/mainSettingsActions";
 
 describe('Menu component', () => {
   it('renders without crashing', () => {
@@ -15,11 +17,22 @@ describe('Menu component', () => {
 
   it('renders music games', () => {
     renderWithProvider(<Menu type='music' />);
+    expect(screen.getByText("WHAT'S THAT INSTRUMENT?")).toBeInTheDocument();
+    expect(screen.getByText('JUMP INTO RHYTHM')).toBeInTheDocument();
     expect(screen.getByText('SEQUENCE MAKER')).toBeInTheDocument();
+    expect(screen.getByText('WORD-TO-MUSIC DECODER')).toBeInTheDocument();
   });
 
   it('renders art games', () => {
     renderWithProvider(<Menu type='art' />);
     expect(screen.getByText('COLOR THEORY')).toBeInTheDocument();
+    expect(screen.getByText('FREE PAINT')).toBeInTheDocument();
   });
+  
+  it('renders tools', () => {
+    store.dispatch(changeMenu('tools'));
+    renderWithProvider(<Menu />);
+    expect(screen.getByText('SCORE KEEPER')).toBeInTheDocument();
+    expect(screen.getByText('TIME KEEPER')).toBeInTheDocument();
+  })
 });
