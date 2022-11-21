bf4761f88b6de813334629ad86e261e86a8558b0
Adam Thometz <adamthometz@gmail.com>
2022-11-20 19:26:15 -0500

    added toggle between notes and lily pads

--- src/jump-into-rhythm/JumpIntoRhythm.js
+++ src/jump-into-rhythm/JumpIntoRhythm.js
@@ -9,12 +9,14 @@ import WindowNavbar from '_components/window-nav/WindowNavbar';
 import Instructions from '_components/instructions/Instructions';
 import Notes from './notes/Notes';
 import NoteOptions from './note-options/NoteOptions';
+import FrogLilyPad from './frog-lily-pad/FrogLilyPads';
 
 import { JIR } from '_data/_utils/localStorageKeys';
 import activities from '_data/_activities/activityList';
 
 const JumpIntoRhythm = () => {
   const { currGame } = useSelector(state => state.mainSettings);
+  const { isDisplayingLilyPads } = useSelector(state => state.jumpIntoRhythm);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -30,7 +32,7 @@ const JumpIntoRhythm = () => {
     <>
       <WindowNavbar page={currGame.name} />
       {!hasVisited ? <Instructions game={currGame} setHasVisited={setHasVisited} /> : (<>
-        <Notes />
+        {isDisplayingLilyPads ? <FrogLilyPad /> : <Notes />}
         <NoteOptions />
       </>)}
     </>
