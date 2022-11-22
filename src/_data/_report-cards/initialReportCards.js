b3442da9d09c11bf925992760e37131296cd2af5
Adam Thometz <adamthometz@gmail.com>
2022-11-22 15:14:31 -0500

    added default report cards

---
+++ src/_data/_report-cards/initialReportCards.js
@@ -0,0 +1,14 @@
+export const instrumentIdReportCard = {
+  brass: [],
+  electronic: [],
+  percussion: [],
+  strings: [],
+  woodwind: [],
+  voice: []
+};
+
+export const colorTheoryReportCard = {
+  primary: [],
+  secondary: [],
+  tertiary: [],
+};
