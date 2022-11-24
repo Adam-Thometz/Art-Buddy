7bb0034c798bc3b2e4581899654319feebf768cc
Adam Thometz <adamthometz@gmail.com>
2022-11-24 11:01:23 -0500

    wrote getNewSize function

--- src/_helpers/free-paint/getNewSize.js
+++ src/_helpers/free-paint/getNewSize.js
@@ -1,12 +1,12 @@
-export default function getNewSize({ x, y, clientWidth, clientHeight, currSize }) {
-  const xDiff = Math.abs(x - clientWidth);
-  const yDiff = Math.abs(y - clientHeight);
-  const width = x > clientWidth
-    ? currSize.width - xDiff
-    : currSize.width + xDiff;
-  const height = y > clientHeight 
-    ? currSize.height + yDiff
-    : currSize.height - yDiff;
+export default function getNewSize({ x, y, clientX, clientY, currSize }) {
+  const xDiff = Math.abs(x - clientX);
+  const yDiff = Math.abs(y - clientY);
+  const width = x > clientX
+    ? currSize.width + xDiff
+    : currSize.width - xDiff;
+  const height = y > clientY 
+    ? currSize.height - yDiff
+    : currSize.height + yDiff;
 
-    return { height, width };
+  return { height, width };
 }
