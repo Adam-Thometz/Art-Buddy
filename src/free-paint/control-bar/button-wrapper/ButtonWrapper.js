4bcde0400f62f75a13e8cc05ee83da4f318e1389
Adam Thometz <adamthometz@gmail.com>
2022-11-29 10:10:24 -0500

    cleanup

--- src/free-paint/control-bar/button-wrapper/ButtonWrapper.js
+++ src/free-paint/control-bar/button-wrapper/ButtonWrapper.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext } from 'react';
 import ControlBarContext from '_utils/free-paint/ControlBarContext';
 
 import './ButtonWrapper.css';
