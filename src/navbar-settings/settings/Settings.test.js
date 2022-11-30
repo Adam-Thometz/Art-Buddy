6e7d48ddd0a17c62eaa3f23b4a2c0a2ef468a163
Adam Thometz <adamthometz@gmail.com>
2022-11-30 18:57:19 -0500

    imported PopupContextMock

--- src/navbar-settings/settings/Settings.test.js
+++ src/navbar-settings/settings/Settings.test.js
@@ -1,15 +1,20 @@
 import React from "react";
+import { PopupContextMock } from "_testUtils/mocks/contextMocks";
 import renderWithProvider from "_testUtils/renderWithProvider";
 import Settings from './Settings';
 
 describe('Settings component', () => {
   window.localStorage.setItem('rosters', '{}')
   it('renders without crashing', () => {
-    renderWithProvider(<Settings />);
+    renderWithProvider(<PopupContextMock>
+      <Settings />
+    </PopupContextMock>);
   });
 
   it('matches the snapshot of Settings', () => {
-    const { asFragment } = renderWithProvider(<Settings />);
+    const { asFragment } = renderWithProvider(<PopupContextMock>
+      <Settings />
+    </PopupContextMock>);
     expect(asFragment()).toMatchSnapshot();
   });
 });
--- src/navbar-settings/settings/rosters/Rosters.test.js
+++ src/navbar-settings/settings/rosters/Rosters.test.js
@@ -1,15 +1,20 @@
 import React from "react";
+import { PopupContextMock } from "_testUtils/mocks/contextMocks";
 import renderWithProvider from "_testUtils/renderWithProvider";
 import Rosters from './Rosters';
 
 describe('Rosters component', () => {
   window.localStorage.setItem('rosters', '{}');
   it('renders without crashing', () => {
-    renderWithProvider(<Rosters />);
+    renderWithProvider(<PopupContextMock>
+      <Rosters />
+    </PopupContextMock>);
   });
 
   it('matches the snapshot of Rosters', () => {
-    const { asFragment } = renderWithProvider(<Rosters />);
+    const { asFragment } = renderWithProvider(<PopupContextMock>
+      <Rosters />
+    </PopupContextMock>);
     expect(asFragment()).toMatchSnapshot();
   });
 });
