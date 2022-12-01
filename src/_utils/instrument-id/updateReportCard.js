375c12a771bc7be1e6c11cadbd1392cf308a7216
Adam Thometz <adamthometz@gmail.com>
2022-11-30 20:56:53 -0500

    fixed typo

--- src/_utils/instrument-id/updateReportCard.js
+++ src/_utils/instrument-id/updateReportCard.js
@@ -1,6 +1,6 @@
 /* updateReportCard
   Purpose: update an array of correct answers on a report card, discarding any duplicates. Takes an array from a report card called group and a string (normally a correct answer) and returns an array that doesn't contain duplicates
-  Found in: Choices.js
+  Found in: Choice.js
 */
 
 export default function updateReportCard({ group, name }) {
