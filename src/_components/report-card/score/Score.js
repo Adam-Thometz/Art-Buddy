7eb5179b786da11f7e986ea36beb6046460a23ad
Adam Thometz <adamthometz@gmail.com>
2022-12-01 16:28:38 -0500

    added semicolons

--- src/_components/report-card/score/Score.js
+++ src/_components/report-card/score/Score.js
@@ -10,15 +10,16 @@ const Score = ({ name, score, maxScore, colorPalette }) => {
     for (let i = 1; i <= maxScore; i++) {
       const src = i === 1 ? first : (i === maxScore ? last : mid);
       if (i <= score) {
-        const paletteIdx = i === 1 ? 0 : (i === maxScore ? colorPalette.length-1 : 1)
+        const paletteIdx = i === 1 ? 0 : (i === maxScore ? colorPalette.length-1 : 1);
         const filter = colorPalette[paletteIdx];
         scoreDisplay.push(<img src={src} alt='' style={{ filter }} />);
       } else {
         scoreDisplay.push(<img src={src} alt='' />);
-      }
-    }
+      };
+    };
     return scoreDisplay;
-  }
+  };
+
   return (
     <section key={name} className='Score'>
       <span>{name}</span>
