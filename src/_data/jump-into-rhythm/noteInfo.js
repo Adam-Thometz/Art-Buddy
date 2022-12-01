d02dbebc6d9a4e63e5e830f26de092f1d878e32f
Adam Thometz <adamthometz@gmail.com>
2022-12-01 16:32:09 -0500

    removed unecessary notes

--- src/_data/jump-into-rhythm/noteInfo.js
+++ src/_data/jump-into-rhythm/noteInfo.js
@@ -1,9 +1,7 @@
 import {
   quarterNote,
   eighthNotes,
-  eighthNote,
   quarterRest,
-  eighthRest
 } from "_media/jump-into-rhythm/_icons/notes/noteImports";
 
 const noteInfo = {
@@ -19,24 +17,12 @@ const noteInfo = {
     img: eighthNotes,
     duration: ['8n', '8n']
   },
-  eighthNote: {
-    id: 'eighthNotes',
-    text: 'Eighth Note',
-    img: eighthNote,
-    duration: ['8n']
-  },
   quarterRest: {
     id: 'quarterRest',
     text: 'Quarter Rest',
     img: quarterRest,
     duration: ['4n']
   },
-  eighthRest: {
-    id: 'eighthRest',
-    text: 'Eighth Rest',
-    img: eighthRest,
-    duration: ['8n']
-  }
 };
 
 export default noteInfo;
--- src/_media/jump-into-rhythm/_icons/notes/noteImports.js
+++ src/_media/jump-into-rhythm/_icons/notes/noteImports.js
@@ -1,13 +1,9 @@
 import quarterNote from './quarter-note.png';
 import eighthNotes from './eighth-notes.png';
-import eighthNote from './eighth-note.png';
 import quarterRest from './quarter-rest.png';
-import eighthRest from './eighth-rest.png'
 
 export {
   quarterNote,
   eighthNotes,
-  eighthNote,
   quarterRest,
-  eighthRest
 };
