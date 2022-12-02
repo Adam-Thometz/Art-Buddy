f87261a3840d5a7c1c3cf98850c8e4a5b68fcac9
Adam Thometz <adamthometz@gmail.com>
2022-12-02 17:33:29 -0500

    refactor and added hop function

--- src/jump-into-rhythm/note-options/NoteOptions.js
+++ src/jump-into-rhythm/note-options/NoteOptions.js
@@ -9,15 +9,17 @@ import Button from '_components/button/Button';
 import Icon from '_components/icon/Icon';
 
 import noteInfo from '_data/jump-into-rhythm/noteInfo';
+import hop from '_utils/jump-into-rhythm/hop';
 
 const NoteOptions = () => {
   const { rhythm, isDisplayingLilyPads } = useSelector(state => state.jumpIntoRhythm);
+  const { volume } = useSelector(state => state.mainSettings);
   const dispatch = useDispatch();
 
   const handleAddNotes = e => {
     const { id } = e.currentTarget;
-    const { img, duration } = noteInfo[id];
-    dispatch(addToRhythm({ id, img, duration }));
+    const { img, duration, isRest } = noteInfo[id];
+    dispatch(addToRhythm({ id, img, duration, isRest }));
   };
 
   const notes = Object.keys(noteInfo).map(noteId => {
@@ -25,9 +27,13 @@ const NoteOptions = () => {
     return <Icon icon={img} text={text} size='100px' width='30%' id={noteId} onClick={handleAddNotes} />;
   });
 
-  const play = () => {
-    if (rhythm.includes(null)) return;
+  const handlePlay = () => {
     dispatch(toggleLilyPadDisplay());
+    const beats = rhythm.map(note => ({
+      duration: note.duration,
+      isRest: note.isRest
+    }));
+    hop({ beats, volume });
   };
 
   const back = () => dispatch(toggleLilyPadDisplay());
@@ -39,7 +45,7 @@ const NoteOptions = () => {
       </section>
       {isDisplayingLilyPads
         ? <Button onClick={back}>BACK</Button>
-        : <Button colorId={0} disabled={rhythm.includes(null)} onClick={play}>PLAY</Button>
+        : <Button colorId={0} disabled={rhythm.includes(null)} onClick={handlePlay}>PLAY</Button>
       }
     </section>
   );
