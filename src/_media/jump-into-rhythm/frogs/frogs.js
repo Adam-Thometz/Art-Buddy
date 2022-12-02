394377fc048d82539abd28ced70d53bab0f1c2d0
Adam Thometz <adamthometz@gmail.com>
2022-12-02 12:25:08 -0500

    added different colored frogs

---
+++ src/_media/jump-into-rhythm/frogs/frogs.js
@@ -0,0 +1,11 @@
+import blue from './blue.png';
+import green from './green.png';
+import red from './red.png';
+import yellow from './yellow.png';
+
+export {
+  blue,
+  green,
+  red,
+  yellow,
+};
