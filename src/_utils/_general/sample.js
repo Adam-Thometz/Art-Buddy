d510f95498225d97781b17ddc411ea971b6c3519
Adam Thometz <adamthometz@gmail.com>
2022-12-02 17:32:09 -0500

    added dynamic base note

--- src/_utils/_general/sample.js
+++ src/_utils/_general/sample.js
@@ -4,9 +4,9 @@ import { Sampler } from "tone";
  * Purpose: takes a sound and a number for volume and returns a sample to play notes with. Used mainly in play-related functions
  */
 
-export default function sample({ sound, volume, toDestination = true }) {
+export default function sample({ sound, volume, toDestination = true, baseNote = 'C3' }) {
   const sample = new Sampler({
-    urls: { C3: sound }
+    urls: { [baseNote]: sound }
   });
   sample.volume.value = volume;
 
