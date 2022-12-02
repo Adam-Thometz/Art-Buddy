be2794b90154fc056291d42c6d26934a5b8fef70
Adam Thometz <adamthometz@gmail.com>
2022-12-02 17:42:12 -0500

    cleanup

--- src/_utils/jump-into-rhythm/buffers.js
+++ src/_utils/jump-into-rhythm/buffers.js
@@ -3,8 +3,8 @@ import { Buffer } from "tone";
 
 export function createBuffers() {
   window['boing'] = new Buffer(boing);
-}
+};
 
 export function removeBuffers() {
   delete window['boing'];
-}
+};
