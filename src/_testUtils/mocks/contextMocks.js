d65918c3413072c462c5935eb84f9fc96667b847
Adam Thometz <adamthometz@gmail.com>
2022-12-06 14:22:42 -0500

    added mock for SongMakerContext

--- src/_testUtils/mocks/contextMocks.js
+++ src/_testUtils/mocks/contextMocks.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import ControlBarContext from "_utils/free-paint/ControlBarContext"
+import SongMakerInfoContext from "_utils/instrument-id/SongMakerInfoContext";
 import PopupContext from "_utils/settings/PopupContext";
 
 // Used in ButtonWrapper.test.js
@@ -21,3 +22,12 @@ export const PopupContextMock = ({ children }) => {
     </PopupContext.Provider>
   );
 };
+
+export const SongMakerContextMock = ({ children }) => {
+  const [loop, setLoop] = useState(null);
+  return (
+    <SongMakerInfoContext.Provider value={{ loop, setLoop }}>
+      {children}
+    </SongMakerInfoContext.Provider>
+  )
+}
