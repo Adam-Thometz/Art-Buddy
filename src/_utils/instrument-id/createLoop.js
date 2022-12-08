080fcd4d070405095e12edcffd5f9e9cb3377b32
Adam Thometz <adamthometz@gmail.com>
2022-12-08 12:15:28 -0500

    cleanup

--- src/_utils/instrument-id/createLoop.js
+++ src/_utils/instrument-id/createLoop.js
@@ -4,23 +4,21 @@ import * as rhythms from '_media/instrument-id/_melodies-rhythms/rhythms';
 import sample from '_utils/_general/sample';
 import { Part } from 'tone';
 
-/** play:
- * Purpose: used for Song Maker feature. Plays a melody/rhythm using a passed in buffer (or buffers if rhythm)
+/** createLoop:
+ * Purpose: used for Song Maker feature. Creates a loop for one instrument
  * Found in: SongMaker.js, SavedSongs.js
  */
 
- export default function createLoop({ melodyId, volume, buffers, isRhythm }) {
+export default function createLoop({ melodyId, volume, buffers, isRhythm }) {
   const instrument = isRhythm ?
     buffers.map(b => sample({ sound: b, volume })) :
     sample({ sound: buffers, volume });
   const melody = isRhythm ? rhythms[melodyId] : melodies[melodyId];
 
   const part = new Part(((time, value) => {
-    if (isRhythm) {
-      value.notes.forEach(note => instrument[note].triggerAttackRelease('C3', value.duration, time));
-    } else {
-      instrument.triggerAttackRelease(value.notes, value.duration, time);
-    }
+    isRhythm
+      ? value.notes.forEach(note => instrument[note].triggerAttackRelease('C3', value.duration, time))
+      : instrument.triggerAttackRelease(value.notes, value.duration, time);
   }), melody);
 
   part.loop = true;
