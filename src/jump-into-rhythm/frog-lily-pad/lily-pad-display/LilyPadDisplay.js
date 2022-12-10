c95715ba08fb0b3f7bff365863d3d4ec91f19258
Adam Thometz <adamthometz@gmail.com>
2022-12-10 11:24:12 -0500

    removed unecessary component

--- src/jump-into-rhythm/frog-lily-pad/lily-pad-display/LilyPadDisplay.js
+++ src/jump-into-rhythm/frog-lily-pad/lily-pad-display/LilyPadDisplay.js
@@ -2,12 +2,27 @@ import React from 'react';
 
 import './LilyPadDisplay.css';
 
-import LilyPad from './lily-pad/LilyPad';
+import lilyPad from '_media/jump-into-rhythm/_icons/lily-pad.png';
+import flower from '_media/jump-into-rhythm/_icons/lily-pad-flower.png';
 
 const LilyPadDisplay = ({ measure }) => {
   return (
     <div className='LilyPadDisplay'>
-      {measure.map(beat => <LilyPad beat={beat} />)}
+      {measure.map(beat => (
+        <div className='LilyPadDisplay-lily-pad'>
+          {beat ? (
+            beat.duration.map(note => (<>
+              <img
+                className='LilyPadDisplay-pad'
+                src={lilyPad}
+                alt='Lily pad'
+                style={{ width: note === '8n' || beat.isRest ? '45%' : '90%' }}
+              />
+              {beat.isRest ? <img className='LilyPadDisplay-flower' src={flower} alt='' /> : null}
+            </>))
+          ) : null}
+        </div>
+      ))}
     </div>
   );
 };
