5e4d035655c0461e23979b9c663f32acd40d7646
Adam Thometz <adamthometz@gmail.com>
2022-12-13 21:40:26 -0500

    refactor

--- src/_utils/instrument-id/calculateTimeLeft.js
+++ src/_utils/instrument-id/calculateTimeLeft.js
@@ -1,9 +1,9 @@
 import { Time } from "tone";
 
-const MOST_POSSIBLE_TIME = Time('4m').toSeconds();
+const MOST_POSSIBLE_TIME = Time('4m').toSeconds() * 1000;
 
 export default function calculateTimeLeft(loop) {
-  const progress = loop.map(part => (MOST_POSSIBLE_TIME * part.progress) * 1000);
+  const progress = loop.map(part => MOST_POSSIBLE_TIME * part.progress);
   const timeLeft = (progress.reduce((accum, value) => accum+value, 0)) / progress.length;
-  return  MOST_POSSIBLE_TIME*1000 - timeLeft;
+  return MOST_POSSIBLE_TIME - timeLeft;
 }
