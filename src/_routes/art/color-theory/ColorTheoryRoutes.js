9cf511bc2f3a92fb92a44c3c060628b2edc714a9
Adam Thometz <adamthometz@gmail.com>
2022-12-18 12:33:56 -0500

    added LearnColors page to routes

--- src/_routes/art/color-theory/ColorTheoryRoutes.js
+++ src/_routes/art/color-theory/ColorTheoryRoutes.js
@@ -2,8 +2,8 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 
 import Window from "_components/window/Window";
-
 import Learn from "color-theory/learn/Learn";
+import LearnColors from "color-theory/learn/colors/LearnColors";
 
 import { colorTheoryUrls } from "_data/_routes/routeUrls";
 
@@ -12,7 +12,7 @@ const ColorTheoryRoutes = () => {
     <Routes>
       {/* Learn */}
       <Route path={colorTheoryUrls.learn} element={<Window><Learn /></Window>} />
-      <Route path={colorTheoryUrls.learnCategory} element={<Window></Window>} />
+      <Route path={colorTheoryUrls.learnCategory} element={<Window><LearnColors /></Window>} />
     </Routes>
   );
 };
