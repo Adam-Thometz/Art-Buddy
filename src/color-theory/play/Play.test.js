6bcf36c6952a5c190dc809b78aae22b3c842254d
Adam Thometz <adamthometz@gmail.com>
2022-12-18 21:45:22 -0500

    added test

---
+++ src/color-theory/play/Play.test.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import renderWithProvider from "_testUtils/renderWithProvider";
+
+import Play from "./Play";
+
+describe('Play component', () => {
+  it('renders without crashing', () => {
+    renderWithProvider(<Play />);
+  });
+
+  it('matches snapshot', () => {
+    const { asFragment } = renderWithProvider(<Play />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
