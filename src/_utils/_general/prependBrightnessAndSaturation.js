8dde04cdb4e79234b18c0e5a227989f3671366d6
Adam Thometz <adamthometz@gmail.com>
2022-12-19 08:38:29 -0500

    cleanup

--- src/_utils/_general/prependBrightnessAndSaturation.js
+++ src/_utils/_general/prependBrightnessAndSaturation.js
@@ -1,6 +1,6 @@
 // function to convert gray block to black so the filter can work
-// takes multiple colors and returns an array of colors with
-// brightness and saturation added.
+// takes the filter settings of a color and returns the filter with
+// brightness and saturation added at the beginning.
 export default function prependBrightnessAndSaturation(color) {
   return `brightness(0%) saturate(100%) ${color}`
 };
