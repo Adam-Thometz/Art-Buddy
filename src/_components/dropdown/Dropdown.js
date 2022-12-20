2b8f3464272225653b45c613136d1123e2d9e88a
Adam Thometz <adamthometz@gmail.com>
2022-12-20 16:17:51 -0500

    fixed zindex issue with dropdown

--- src/_components/dropdown/Dropdown.js
+++ src/_components/dropdown/Dropdown.js
@@ -16,18 +16,28 @@ const Dropdown = ({ id = null, labelText, onClick, options, borderColor }) => {
   };
 
   const optionsMenu = (
-    <div className='Dropdown-options' onClick={onClick} id={id !== null ? id : null}>
+    <div
+      className='Dropdown-options'
+      style={{ zIndex: showOptions ? 6 : 4 }}
+      onClick={onClick}
+      id={id !== null ? id : null}
+    >
       {Object.keys(options).map((value) => (
         <p key={value} className='Dropdown-option' onClick={handleChangeLabel} id={value}>{options[value]}</p>
       ))}
     </div>
   );
 
+  const labelStyle = {
+    zIndex: showOptions ? 7 : 5,
+    borderColor
+  };
+
   const downArrow = <img src={downArrowIcon} alt='' />;
   
   return (
     <div className="Dropdown">
-      <span className='Dropdown-label' onClick={handleShowOptions} style={borderColor}>{displayLabelText} {downArrow}</span>
+      <span className='Dropdown-label' onClick={handleShowOptions} style={labelStyle}>{displayLabelText} {downArrow}</span>
       {showOptions ? optionsMenu : null}
     </div>
   );
