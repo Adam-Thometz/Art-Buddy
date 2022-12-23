f8f3cc673713ca1bb2f1044a7ae5bed1886fde2d
Adam Thometz <adamthometz@gmail.com>
2022-12-22 22:42:17 -0500

    added tests for sample and createSound

---
+++ src/_utils/music-decoder/createSound.test.js
@@ -0,0 +1,43 @@
+import createSound from "./createSound";
+
+jest.mock('tone', () => ({
+  PitchShift(pitch) {
+    return {
+      toDestination() {
+        return pitch;
+      },
+    };
+  },
+  Synth() {
+    return {
+      connect(pitch) {
+        return { pitch, sound: 'synth', volume: {} };
+      },
+    };
+  },
+  Sampler({ urls }) {
+    return {
+      connect(pitch) {
+        return { pitch, volume: { value: 0 } };
+      },
+    };
+  },
+}));
+
+describe('createSound function', () => {
+  it('should create a synth sound by default', () => {
+    const instrument = createSound({ volume: 0 });
+    expect(instrument).toEqual({ pitch: 0, sound: 'synth', volume: { value: 0 } });
+  });
+  
+  it('should create a sound in a new scale', () => {
+    const instrument = createSound({ volume: 0, scale: 6 });
+    expect(instrument).toEqual({ pitch: 6, sound: 'synth', volume: { value: 0 } });
+  });
+  
+  // TODO: Make this test work
+  // it('should create a sample out of a sound', () => {
+  //   const instrument = createSound({ volume: 0, sampleId: 'trumpet' });
+  //   expect(instrument).toEqual({ pitch: 0, sound: 'trumpet', volume: { value: 0 } })
+  // });
+});
