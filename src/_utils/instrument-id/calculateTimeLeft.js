20f519401d1bad258692b36aef7a153190cb826a
Adam Thometz <adamthometz@gmail.com>
2022-12-24 00:39:46 -0500

    added documentation to function

--- src/_utils/instrument-id/calculateTimeLeft.js
+++ src/_utils/instrument-id/calculateTimeLeft.js
@@ -1,5 +1,9 @@
 import { Time } from "tone";
 
+/** calculateTimeLeft
+ * Calculates the amount of time left before a loop restarts. Useful for resetting timers
+ */
+
 export default function calculateTimeLeft(loop) {
   const MOST_POSSIBLE_TIME = Time('4m').toSeconds() * 1000;
   const progress = loop.map(part => MOST_POSSIBLE_TIME * part.progress);
