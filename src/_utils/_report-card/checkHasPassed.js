db8987ff79731890b17de6e3cf570efa8434bbc1
Adam Thometz <adamthometz@gmail.com>
2022-12-24 00:04:28 -0500

    added documentation

--- src/_utils/_report-card/checkHasPassed.js
+++ src/_utils/_report-card/checkHasPassed.js
@@ -1,5 +1,9 @@
+/** checkHasPassed
+ * checks a report card to see if it has a perfect score. Used to unlock new levels and other things.
+ */
+
 export default function checkHasPassed(reportCard) {
   return Object.keys(reportCard).every(score => (
-    reportCard[score].results.length === reportCard[score].maxScore
-  )); 
+    reportCard[score].results.length >= reportCard[score].maxScore
+  ));
 };
