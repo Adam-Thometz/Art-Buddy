42c5c0b86ecfa231454b8624feeb9b62dc34e50d
Adam Thometz <adamthometz@gmail.com>
2022-12-24 00:04:46 -0500

    added test

--- src/_utils/_report-card/checkHasPassed.test.js
+++ src/_utils/_report-card/checkHasPassed.test.js
@@ -6,4 +6,14 @@ describe('checkHasPassed function', () => {
     const result = checkHasPassed(instrumentIdReportCard);
     expect(result).toBe(false);
   });
+
+  it('should return true if a report card has all passing scores', () => {
+    const passingReportCard = { ...instrumentIdReportCard };
+    for (let family in passingReportCard) {
+      const { maxScore } = passingReportCard[family];
+      passingReportCard[family].results = Array(maxScore);
+    };
+    const result = checkHasPassed(passingReportCard);
+    expect(result).toBe(true);
+  });
 });
