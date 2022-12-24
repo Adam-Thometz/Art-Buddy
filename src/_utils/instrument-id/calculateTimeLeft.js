566a037fff779c29178a3f32a11c21b7d7fcdfd0
Adam Thometz <adamthometz@gmail.com>
2022-12-24 00:36:57 -0500

    cleanup

--- src/_utils/instrument-id/calculateTimeLeft.js
+++ src/_utils/instrument-id/calculateTimeLeft.js
@@ -5,4 +5,4 @@ export default function calculateTimeLeft(loop) {
   const progress = loop.map(part => MOST_POSSIBLE_TIME * part.progress);
   const timeLeft = progress.reduce((accum, value) => accum+value, 0) / progress.length;
   return MOST_POSSIBLE_TIME - timeLeft;
-}
+};
