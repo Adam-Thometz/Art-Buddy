cdc99d2912d2155fe54a30741acf1b62727bf847
Adam Thometz <adamthometz@gmail.com>
2022-12-24 00:21:13 -0500

    reorganize

--- src/_routes/music/instrument-id/InstrumentIdRoutes.js
+++ src/_routes/music/instrument-id/InstrumentIdRoutes.js
@@ -3,10 +3,10 @@ import { Route, Routes } from "react-router-dom";
 
 import Window from "_components/window/Window";
 
-import LearnInstrumentId from "instrument-id/learn/learn-main/LearnInstrumentId";
+import LearnInstrumentId from "instrument-id/learn/LearnInstrumentId";
 import LearnFamilyPage from "instrument-id/learn/family/LearnFamilyPage";
 import Instrument from "instrument-id/learn/instrument/Instrument";
-import Play from 'instrument-id/play/PlayInstrumentId';
+import PlayInstrumentId from 'instrument-id/play/PlayInstrumentId';
 import ListeningSkills from "instrument-id/play/listening-skills/ListeningSkills";
 import ListeningSkillsTest from "instrument-id/play/listening-skills/level/ListeningSkillsTest";
 import SongMaker from "instrument-id/play/song-maker/SongMaker";
@@ -21,7 +21,7 @@ const InstrumentIdRoutes = () => {
       <Route path={instrumentIdUrls.learnFamily} element={<Window><LearnFamilyPage /></Window>}/>
       <Route path={instrumentIdUrls.learnInstrument} element={<Window><Instrument /></Window>}/>
       {/* Play */}
-      <Route path={instrumentIdUrls.play} element={<Window><Play /></Window>} />
+      <Route path={instrumentIdUrls.play} element={<Window><PlayInstrumentId /></Window>} />
       <Route path={instrumentIdUrls.playListening} element={<Window><ListeningSkills /></Window>} />
       <Route path={instrumentIdUrls.playListeningLevel} element={<Window><ListeningSkillsTest /></Window>} />
       <Route path={instrumentIdUrls.playSongMaker} element={<Window><SongMaker /></Window>} />
