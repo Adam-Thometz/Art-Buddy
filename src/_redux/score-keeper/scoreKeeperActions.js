38ed64a6ba8769e314935bf6764cc658d78a407d
Adam Thometz <adamthometz@gmail.com>
2022-12-27 16:15:30 -0500

    removed unnecessary actions

--- src/_redux/score-keeper/scoreKeeperActions.js
+++ src/_redux/score-keeper/scoreKeeperActions.js
@@ -1,8 +1,5 @@
 import { createAction } from "@reduxjs/toolkit";
 
 export const loadStudents = createAction('loadStudents');
-export const addPoint = createAction('addPoint');
-export const removePoint = createAction('removePoint');
 export const updatePoints = createAction('updatePoints');
-export const resetScores = createAction('resetScores');
 export const clearGame = createAction('clearGame');
