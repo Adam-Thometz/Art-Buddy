3befd12900dc071af502d080bb67fc19bbb4ec36
Adam Thometz <adamthometz@gmail.com>
2022-12-31 11:45:41 -0500

    dried up

--- src/_utils/sequence-maker/createSounds.test.js
+++ src/_utils/sequence-maker/createSounds.test.js
@@ -22,50 +22,19 @@ jest.mock('tone', () => ({
 }));
 
 const sequence = [{ sound: 'file' }, { sound: 'file' }, { sound: 'file' }, null];
+const { sounds, playOne, playSequence } = createSounds(sequence);
 
 describe('createSounds function', () => {
   it('should create an array of sounds from the sequence', () => {
-    const result = createSounds(sequence);
-    expect(Object.keys(result).length).toBe(3);
-    const { sounds } = result
     expect(sounds[0].urls).toEqual({ C3: 'file' });
     expect(sounds[1].urls).toEqual({ C3: 'file' });
     expect(sounds[2].urls).toEqual({ C3: 'file' });
     expect(sounds[3]).toEqual(null);
   });
-  
-  it('should play a single sound', () => {
-    const { playOne } = createSounds(sequence);
-    const result = playOne({ id: 0, pitch: 4, duration: 3 });
-    expect(result).toEqual({ note: 'C4', duration: 3, time: 0 });
-  });
-
-  it('should play multiple sounds one at a time', () => {
-    const { playSequence } = createSounds(sequence);
-    const result = playSequence({ pitch: 4, duration: 3, playAll: false });
-    expect(result.notesToPlay).toEqual([
-      { note: 'C4', time: 0, sound: expect.any(Object) },
-      { note: 'C4', time: 3, sound: expect.any(Object) },
-      { note: 'C4', time: 6, sound: expect.any(Object) },
-      { note: 'C4', time: 9, sound: null },
-    ]);
-  });
-  
-  it('should play multiple sounds all at once', () => {
-    const { playSequence } = createSounds(sequence);
-    const result = playSequence({ pitch: 2, duration: 3, playAll: true });
-    expect(result.notesToPlay).toEqual([
-      { note: 'C2', time: 0, sound: expect.any(Object) },
-      { note: 'C2', time: 0, sound: expect.any(Object) },
-      { note: 'C2', time: 0, sound: expect.any(Object) },
-      { note: 'C2', time: 0, sound: null },
-    ]);
-  });
 });
 
 describe('playOne function', () => {
   it('should play a single sound', () => {
-    const { playOne } = createSounds(sequence);
     const result = playOne({ id: 0, pitch: 4, duration: 3 });
     expect(result).toEqual({ note: 'C4', duration: 3, time: 0 });
   });
@@ -73,7 +42,6 @@ describe('playOne function', () => {
 
 describe('playSequence function', () => {
   it('should play multiple sounds one at a time', () => {
-    const { playSequence } = createSounds(sequence);
     const result = playSequence({ pitch: '4', duration: 3, playAll: false });
     expect(result.notesToPlay).toEqual([
       { note: 'C4', time: 0, sound: expect.any(Object) },
@@ -84,7 +52,6 @@ describe('playSequence function', () => {
   });
   
   it('should play multiple sounds all at once', () => {
-    const { playSequence } = createSounds(sequence);
     const result = playSequence({ pitch: '4', duration: 3, playAll: true });
     expect(result.notesToPlay).toEqual([
       { note: 'C4', time: 0, sound: expect.any(Object) },
