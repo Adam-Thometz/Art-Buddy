a721da82f13e269037b9de8dc3fb2915987b7299
Adam Thometz <adamthometz@gmail.com>
2022-12-31 11:35:55 -0500

    made test for loadSounds

---
+++ src/_utils/instrument-id/loadSounds.test.js
@@ -0,0 +1,57 @@
+import loadSounds from "./loadSounds";
+
+jest.mock('tone', () => ({
+  Sampler: jest.fn(() => ({
+    toDestination: jest.fn()
+  })),
+  Part: jest.fn((_, notesToPlay) => ({
+    notesToPlay,
+    start: jest.fn(),
+    stop: jest.fn(),
+  })),
+  now: jest.fn(() => 0),
+  Transport: {
+    start: jest.fn(),
+    stop: jest.fn(),
+  },
+}));
+
+describe('loadSounds function', () => {
+  it('should load a scale for non-rhythmic instruments', () => {
+    const { sounds } = loadSounds({ ids: ['synthesizer'], volume: 0, isTest: false });
+    expect(sounds.synthesizer).toEqual(expect.any(Function));
+  });
+  
+  it('should load a beat for rhythmic instruments', () => {
+    const { sounds } = loadSounds({ ids: ['drumSet'], volume: 0, isTest: false });
+    expect(sounds.drumSet).toEqual(expect.any(Function));
+  });
+});
+
+describe('play function', () => {
+  it('should play a scale for non-rhythmic instruments when isTest is false', () => {
+    const { play } = loadSounds({ ids: ['synthesizer'], volume: 0, isTest: false });
+    const { notesToPlay } = play(['synthesizer']);
+    expect(notesToPlay.length).toBe(8);
+    expect(notesToPlay.map(note => note.note)).toEqual(['C3', 'D3', 'E3', 'F3', 'G3', 'A3', 'B3', 'C4']);
+  });
+
+  it('should play a beat for rhythmic instruments when isTest is false', () => {
+    const { play } = loadSounds({ ids: ['drumSet'], volume: 0, isTest: false });
+    const { notesToPlay } = play(['drumSet']);
+    expect(notesToPlay.length).toBe(10);
+  });
+
+  it('should play a scale for non-rhythmic instruments when isTest is true', () => {
+    const { play } = loadSounds({ ids: ['synthesizer'], volume: 0, isTest: true });
+    const { notesToPlay } = play(['synthesizer']);
+    expect(notesToPlay.length).toBe(5);
+    expect(notesToPlay.map(note => note.note)).toEqual(['C3', 'D3', 'E3', 'F3', 'G3']);
+  });
+
+  it('should play a beat for rhythmic instruments when isTest is true', () => {
+    const { play } = loadSounds({ ids: ['drumSet'], volume: 0, isTest: true });
+    const { notesToPlay } = play(['drumSet']);
+    expect(notesToPlay.length).toBe(5);
+  });
+});
