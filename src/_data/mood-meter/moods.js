e3368d49553fda6a1f95346b6666df75355391a8
Adam Thometz <adamthometz@gmail.com>
2023-01-01 22:44:11 -0500

    created moods data structure

---
+++ src/_data/mood-meter/moods.js
@@ -0,0 +1,14 @@
+/** Data Structure for moods
+ * A 2D array that maps out emotions.
+ * Valence is aquired on the y axis, energy on the x axis
+ * eg. const mood = moods[0][2]
+ * console.log(mood) => 'Angry'
+ */
+
+const moods = [
+  ['Sad',     'Disappointed', 'Angry'],
+  ['Tired',   'Bored',        'Curious'],
+  ['Relaxed', 'Happy',        'Excited'],
+];
+
+export default moods;
