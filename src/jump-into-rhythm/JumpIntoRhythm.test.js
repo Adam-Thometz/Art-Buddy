59132edad7fe442ddaadb0804ddb12309115e2a2
Adam Thometz <adamthometz@gmail.com>
2023-01-04 13:50:43 -0500

    added test

--- src/jump-into-rhythm/JumpIntoRhythm.test.js
+++ src/jump-into-rhythm/JumpIntoRhythm.test.js
@@ -1,6 +1,8 @@
 import React from "react";
 
 import renderWithProvider from "_testUtils/renderWithProvider";
+import { screen, within } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import JumpIntoRhythm from "./JumpIntoRhythm";
 
@@ -30,6 +32,15 @@ describe('JumpIntoRhythm component', () => {
   });
 
   it('should select a musical note', () => {
-    
-  })
+    renderWithProvider(<PlayContextMock>
+      <JumpIntoRhythm />
+    </PlayContextMock>);
+    const options = screen.getAllByTestId('notes');
+    userEvent.click(options[0]);
+    const blocks = screen.getAllByTestId('block');
+    const firstBlock = within(blocks[0]).queryAllByRole('img');
+    const secondBlock = within(blocks[1]).queryAllByRole('img');
+    expect(firstBlock.length).toBe(2);
+    expect(secondBlock.length).toBe(0);
+  });
 });
