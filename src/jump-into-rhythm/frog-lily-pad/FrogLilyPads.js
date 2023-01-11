a312b35907a6068a6022dfe3cb15e755d70d5004
Adam Thometz <adamthometz@gmail.com>
2023-01-11 16:28:10 -0500

    added key

--- src/jump-into-rhythm/frog-lily-pad/FrogLilyPads.js
+++ src/jump-into-rhythm/frog-lily-pad/FrogLilyPads.js
@@ -16,19 +16,19 @@ const FrogLilyPad = () => {
     <section className='FrogLilyPads'>
       <div className='FrogLilyPads-section'>
         <Frog frog={blue} measure={rhythm.slice(0, 4)} />
-        <LilyPadDisplay measure={rhythm.slice(0, 4)} />
+        <LilyPadDisplay measure={rhythm.slice(0, 4)} id={1} />
       </div>
       {rhythm.length >= 8 ? <div className='FrogLilyPads-section'>
         <Frog frog={green} measure={rhythm.slice(4, 8)} delay={Time('1m').toSeconds()} />
-        <LilyPadDisplay measure={rhythm.slice(4, 8)} />
+        <LilyPadDisplay measure={rhythm.slice(4, 8)} id={2} />
       </div> : null}
       {rhythm.length >= 12 ? <div className='FrogLilyPads-section'>
         <Frog frog={red} measure={rhythm.slice(8, 12)} delay={Time('2m').toSeconds()} />
-        <LilyPadDisplay measure={rhythm.slice(8, 12)} />
+        <LilyPadDisplay measure={rhythm.slice(8, 12)} id={3} />
       </div> : null}
       {rhythm.length === 16 ? <div className='FrogLilyPads-section'>
         <Frog frog={yellow} measure={rhythm.slice(12, 16)} delay={Time('3m').toSeconds()} />
-        <LilyPadDisplay measure={rhythm.slice(12, 16)} />
+        <LilyPadDisplay measure={rhythm.slice(12, 16)} id={4} />
       </div> : null}
     </section>
   );
--- src/jump-into-rhythm/frog-lily-pad/lily-pad-display/LilyPadDisplay.js
+++ src/jump-into-rhythm/frog-lily-pad/lily-pad-display/LilyPadDisplay.js
@@ -1,17 +1,17 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 import './LilyPadDisplay.css';
 
 import lilyPad from '_media/jump-into-rhythm/_icons/lily-pad.png';
 import flower from '_media/jump-into-rhythm/_icons/lily-pad-flower.png';
 
-const LilyPadDisplay = ({ measure }) => {
+const LilyPadDisplay = ({ measure, id }) => {
   return (
     <div className='LilyPadDisplay'>
       {measure.map((beat, i) => (
         <div key={`beat${i}`} className='LilyPadDisplay-lily-pad'>
           {beat ? (
-            beat.duration.map(note => (<>
+            beat.duration.map(note => (<Fragment key={`beat${i}-${id}`}>
               <img
                 className='LilyPadDisplay-pad'
                 src={lilyPad}
@@ -19,7 +19,7 @@ const LilyPadDisplay = ({ measure }) => {
                 style={{ width: note === '8n' || beat.isRest ? '45%' : '90%' }}
               />
               {beat.isRest ? <img className='LilyPadDisplay-flower' src={flower} alt='' /> : null}
-            </>))
+            </Fragment>))
           ) : null}
         </div>
       ))}
