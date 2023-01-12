a224b6624723c740e1b7979d82d583f9225f83d2
Adam Thometz <adamthometz@gmail.com>
2023-01-12 11:04:49 -0500

    added popup for video

--- src/mood-meter/result/Result.js
+++ src/mood-meter/result/Result.js
@@ -1,19 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { PopupContext } from '_context/PopupContext';
 
 import { useSelector } from 'react-redux';
 
 import './Result.css';
 
+import Button from '_components/button/Button';
+import Video from '_components/video/Video';
 import Face from 'mood-meter/valence/face/Face';
 import Battery from 'mood-meter/energy/battery/Battery';
 
 import faces from '_data/mood-meter/faces';
 import batteries from '_data/mood-meter/batteries';
 import moods from '_data/mood-meter/moods';
-import Button from '_components/button/Button';
 
 const Result = () => {
   const { valence, energy } = useSelector(state => state.moodMeter);
+  const { setCurrPopup } = useContext(PopupContext);
 
   const face = faces.find(face => face.id === valence);
   const valenceIcon = <Face key='face' src={face.src} id={face.id} isSelected={true} />;
@@ -22,6 +25,11 @@ const Result = () => {
   const energyIcon = <Battery key='battery' width={battery.width} id={battery.id} filter={battery.filter} />;
   
   const mood = moods[valence][energy];
+
+  const openVideo = () => setCurrPopup({
+    title: `${mood.name.toUpperCase()} SONG`,
+    popup: <Video uri={mood.songURI} />
+  })
   
   return (
     <div className='Result'>
@@ -30,7 +38,7 @@ const Result = () => {
         {energyIcon}
       </div>
       <p className='Result-text'>{mood.name}</p>
-      <Button colorId={2}>Listen To Song</Button>
+      <Button colorId={2} onClick={openVideo}>Listen To Song</Button>
     </div>
   );
 };
