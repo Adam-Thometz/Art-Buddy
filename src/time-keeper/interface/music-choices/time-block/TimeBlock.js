c1ad61e1b3addc51c7d225ad7ebd36a3676a52ae
Adam Thometz <adamthometz@gmail.com>
2023-03-10 18:39:19 -0500

    added ability to add songs

--- src/time-keeper/interface/music-choices/time-block/TimeBlock.js
+++ src/time-keeper/interface/music-choices/time-block/TimeBlock.js
@@ -1,8 +1,17 @@
+import { useDispatch } from "react-redux";
+import { addOneChunk } from "_redux/time-keeper/timeKeeperActions";
 import "./TimeBlock.css";
 
-const TimeBlock = ({ mood, seconds }) => {
+const TimeBlock = ({ mood, seconds, music }) => {
+  const dispatch = useDispatch();
+
+  const handleAddSong = () => dispatch(addOneChunk({ music, seconds }));
   const displayValue = seconds >= 60 ? `${seconds / 60} MIN` : `${seconds} SEC`;
-  return <div className={`TimeBlock ${mood}`}>{displayValue}</div>;
+  return (
+    <div className={`TimeBlock ${mood}`} onClick={handleAddSong}>
+      {displayValue}
+    </div>
+  );
 };
 
 export default TimeBlock;
