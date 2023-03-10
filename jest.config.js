6974b3de4e1a21f3a3c8a050c38a77a72ce02d8a
Adam Thometz <adamthometz@gmail.com>
2023-03-10 15:18:43 -0500

    config and cleanup

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,14 @@
 module.exports = {
   moduleNameMapper: {
-    '\\.(css)$': '<rootDir>/src/_testUtils/mocks/styleMock.js',
-    '\\.(mp3|png|jpeg)$': '<rootDir>/src/_testUtils/mocks/fileMock.js',
+    "\\.(css)$": "<rootDir>/src/_testUtils/mocks/styleMock.js",
+    "\\.(mp3|png|jpeg)$": "<rootDir>/src/_testUtils/mocks/fileMock.js",
     // used for relative imports
-    'src(.*)$': "<rootDir>/src$1"
+    "src(.*)$": "<rootDir>/src$1",
   },
-  testEnvironment: 'jsdom',
+  testEnvironment: "jsdom",
   setupFilesAfterEnv: [
-    '<rootDir>/src/setupTests.js',
-    '<rootDir>/src/_testUtils/mocks/localStorageMock.js',
-    'jest-date-mock'
+    "<rootDir>/src/setupTests.js",
+    "<rootDir>/src/_testUtils/mocks/localStorageMock.js",
+    "jest-date-mock",
   ],
 };
