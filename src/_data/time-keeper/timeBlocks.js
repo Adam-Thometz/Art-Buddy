87677ea383767c16a88117fff62b193ef7246c3d
Adam Thometz <adamthometz@gmail.com>
2023-03-12 17:55:00 -0400

    refactor

--- src/_data/time-keeper/timeBlocks.js
+++ src/_data/time-keeper/timeBlocks.js
@@ -2,27 +2,31 @@ import excited15sec from "_media/time-keeper/music/excited/15-sec.mp3";
 import excited30sec from "_media/time-keeper/music/excited/30-sec.mp3";
 import excited1min from "_media/time-keeper/music/excited/1-min.mp3";
 
-const timeBlocks = [
+export const regularTimeBlocks = [
   {
     seconds: 15,
-    excited: excited15sec,
+    excited: [excited15sec],
   },
   {
     seconds: 30,
-    excited: excited30sec,
+    excited: [excited30sec],
   },
   {
     seconds: 60,
-    excited: excited1min,
-  },
-  {
-    seconds: 180,
-    excited: excited1min,
-  },
-  {
-    seconds: 300,
-    excited: excited1min,
+    excited: [excited1min],
   },
 ];
 
-export default timeBlocks;
+export const allTimeBlocks = [
+  ...regularTimeBlocks,
+  {
+    seconds: 180,
+    excited: [],
+  },
+  {
+    seconds: 300,
+    excited: [],
+  },
+];
+
+export default allTimeBlocks;
