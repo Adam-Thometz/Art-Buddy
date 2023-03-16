841addcf58eb43632bcbc685ffff4798ef5434ca
Adam Thometz <adamthometz@gmail.com>
2023-03-16 16:08:36 -0400

    refactor test

--- src/_utils/free-paint/getNewSize.test.js
+++ src/_utils/free-paint/getNewSize.test.js
@@ -1,27 +1,13 @@
 import getNewSize from './getNewSize';
 
 describe('getNewSize function', () => {
-  it('should return a new height and width - clients are larger', () => {
-    const inputs = {
-      x: 100,
-      y: 100,
-      clientX: 105,
-      clientY: 110,
-      currSize: { width: 50, height: 50 },
-    };
-    const result = getNewSize(inputs);
-    expect(result).toEqual({ height: 60, width: 45 });
-  });
-
-  it('should return a new height and width - clients are smaller', () => {
-    const inputs = {
-      x: 100,
-      y: 100,
-      clientX: 95,
-      clientY: 90,
-      currSize: { width: 50, height: 50 },
-    };
-    const result = getNewSize(inputs);
-    expect(result).toEqual({ height: 40, width: 55 });
+  const tests = [
+    // assertion        x    y    clientX cleintY currSize                   result
+    ["larger clients",  100, 100, 105,    110,    { width: 50, height: 50 }, { height: 60, width: 45 }],
+    ["smaller clients", 100, 100, 95,     90,     { width: 50, height: 50 }, { height: 40, width: 55 }]
+  ];
+  it.each(tests)("return a new height and width - %s", (_, x, y, clientX, clientY, currSize, result) => {
+    const inputs = { x, y, clientX, clientY, currSize };
+    expect(getNewSize(inputs)).toEqual(result);
   });
 });
