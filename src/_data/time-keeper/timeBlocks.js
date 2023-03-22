4af9c78d363a64409b4ed0b634351dfc43594c94
Adam Thometz <adamthometz@gmail.com>
2023-03-21 21:35:11 -0400

    added relaxed music

--- src/_data/time-keeper/timeBlocks.js
+++ src/_data/time-keeper/timeBlocks.js
@@ -2,18 +2,25 @@ import excited15sec from "_media/time-keeper/music/excited/15-sec.mp3";
 import excited30sec from "_media/time-keeper/music/excited/30-sec.mp3";
 import excited1min from "_media/time-keeper/music/excited/1-min.mp3";
 
+import relaxed15sec from "_media/time-keeper/music/relaxed/15-sec.mp3";
+import relaxed30sec from "_media/time-keeper/music/relaxed/30-sec.mp3";
+import relaxed1min from "_media/time-keeper/music/relaxed/1-min.mp3";
+
 export const regularTimeBlocks = [
   {
     seconds: 15,
     excited: excited15sec,
+    relaxed: relaxed15sec,
   },
   {
     seconds: 30,
     excited: excited30sec,
+    relaxed: relaxed30sec,
   },
   {
     seconds: 60,
     excited: excited1min,
+    relaxed: relaxed1min,
   },
 ];
 
@@ -22,10 +29,12 @@ export const allTimeBlocks = [
   {
     seconds: 180,
     excited: null,
+    relaxed: null,
   },
   {
     seconds: 300,
     excited: null,
+    relaxed: null,
   },
 ];
 
