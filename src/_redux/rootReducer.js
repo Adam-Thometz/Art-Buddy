51c34360e793f132410aaa42d0dbb7b90e319b27
Adam Thometz <adamthometz@gmail.com>
2023-04-04 16:51:17 -0400

    cleanup

--- src/_redux/rootReducer.js
+++ src/_redux/rootReducer.js
@@ -1,39 +1,39 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 // General/Settings
-import generalReducer from "./_general/generalReducer";
-import settingsReducer from "./settings/settingsReducer";
+import general from "./_general/generalReducer";
+import settings from "./settings/settingsReducer";
 
 // Music Games
-import musicDecoderReducer from "./music-decoder/musicDecoderReducer";
-import jumpIntoRhythmReducer from "./jump-into-rhythm/jumpIntoRhythmReducer";
-import sequenceMakerReducer from "./sequence-maker/sequenceMakerReducer";
-import listeningSkillsTestReducer from "./instrument-id/listening-skills/listeningSkillsTestReducer";
-import songMakerReducer from "./instrument-id/song-maker/songMakerReducer";
+import musicDecoder from "./music-decoder/musicDecoderReducer";
+import jumpIntoRhythm from "./jump-into-rhythm/jumpIntoRhythmReducer";
+import sequenceMaker from "./sequence-maker/sequenceMakerReducer";
+import listeningSkillsTest from "./instrument-id/listening-skills/listeningSkillsTestReducer";
+import songMaker from "./instrument-id/song-maker/songMakerReducer";
 
 // Art Games
-import freePaintReducer from "./free-paint/freePaintReducer";
-import colorTheoryReducer from "./color-theory/colorTheoryReducer";
+import freePaint from "./free-paint/freePaintReducer";
+import colorTheory from "./color-theory/colorTheoryReducer";
 
 // Tools
-import scoreKeeperReducer from "./score-keeper/scoreKeeperReducer";
-import moodMeterReducer from "./mood-meter/moodMeterReducer";
-import timeKeeperReducer from "./time-keeper/timeKeeperReducer";
+import scoreKeeper from "./score-keeper/scoreKeeperReducer";
+import moodMeter from "./mood-meter/moodMeterReducer";
+import timeKeeper from "./time-keeper/timeKeeperReducer";
 
 const rootReducer = configureStore({
   reducer: {
-    general: generalReducer,
-    settings: settingsReducer,
-    musicDecoder: musicDecoderReducer,
-    sequenceMaker: sequenceMakerReducer,
-    jumpIntoRhythm: jumpIntoRhythmReducer,
-    listeningSkillsTest: listeningSkillsTestReducer,
-    songMaker: songMakerReducer,
-    freePaint: freePaintReducer,
-    colorTheory: colorTheoryReducer,
-    scoreKeeper: scoreKeeperReducer,
-    moodMeter: moodMeterReducer,
-    timeKeeper: timeKeeperReducer,
+    general,
+    settings,
+    musicDecoder,
+    sequenceMaker,
+    jumpIntoRhythm,
+    listeningSkillsTest,
+    songMaker,
+    freePaint,
+    colorTheory,
+    scoreKeeper,
+    moodMeter,
+    timeKeeper,
   },
 });
 
