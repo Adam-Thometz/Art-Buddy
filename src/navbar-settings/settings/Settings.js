9e663ffd138862772ad477543cf6d3a8fa641925
Adam Thometz <adamthometz@gmail.com>
2023-04-22 12:50:06 -0400

    removed text-to-speech and color blind features

--- src/navbar-settings/settings/Settings.js
+++ src/navbar-settings/settings/Settings.js
@@ -1,8 +1,8 @@
 import "./Settings.css";
 
 import Volume from "./volume/Volume";
-import TextToSpeech from "./text-to-speech/TextToSpeech";
-import ColorBlind from "./color-blind/ColorBlind";
+// import TextToSpeech from "./text-to-speech/TextToSpeech";
+// import ColorBlind from "./color-blind/ColorBlind";
 import Rosters from "./rosters/Rosters";
 
 const Settings = () => {
@@ -11,11 +11,11 @@ const Settings = () => {
       <p className="Settings-text">Volume</p>
       <Volume />
 
-      <p className="Settings-text">Text-to-Speech</p>
+      {/* <p className="Settings-text">Text-to-Speech</p>
       <TextToSpeech />
 
       <p className="Settings-text">Color Blind Mode</p>
-      <ColorBlind />
+      <ColorBlind /> */}
 
       <p className="Settings-text">Roster</p>
       <Rosters />
