5b507a17ee824816ebb27847d4ba201fed3a82cd
Adam Thometz <adamthometz@gmail.com>
2023-05-09 17:27:17 -0400

    moved time-keeping feature to navbar

--- src/time-keeper/time/time-left/TimeLeft.js
+++ src/time-keeper/time/time-left/TimeLeft.js
@@ -1,37 +1,11 @@
-import { useEffect } from "react";
-
-import { useSelector, useDispatch } from "react-redux";
-import { decrementOneSecond } from "_redux/time-keeper/timeKeeperReducer";
-import { setCurrTimer } from "_redux/_general/generalReducer";
+import { useSelector } from "react-redux";
 
 import "./TimeLeft.css";
 
 import createDisplayTime from "_utils/time-keeper/createDisplayTime";
 
 const TimeLeft = () => {
-  const { timer } = useSelector((state) => state.general);
-  const { secondsLeft, isPlaying } = useSelector((state) => state.timeKeeper);
-  const dispatch = useDispatch()
-
-  useEffect(() => {
-    if (isPlaying && !timer) {
-      if (!timer && secondsLeft) {
-        const timeOut = setTimeout(() => {
-          dispatch(decrementOneSecond())
-          clearTimeout(timeOut)
-          setCurrTimer(null);
-        }, 1000);
-        dispatch(setCurrTimer(timeOut));
-      }
-      if (!secondsLeft) {
-        clearTimeout(timer);
-        setCurrTimer(null);
-      }
-    } else if (!isPlaying) {
-      clearTimeout(timer);
-      setCurrTimer(null);
-    }
-  }, [isPlaying, secondsLeft, dispatch, timer])
+  const { secondsLeft } = useSelector((state) => state.timeKeeper);
 
   const timeDisplay = createDisplayTime(secondsLeft);
 
