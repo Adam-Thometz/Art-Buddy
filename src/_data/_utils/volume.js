f2299484907b1d32552b2a3ccc9873a9efcbe030
Adam Thometz <adamthometz@gmail.com>
2023-05-17 16:42:42 -0400

    made volume dynamic

---
+++ src/_data/_utils/volume.js
@@ -0,0 +1,3 @@
+export const LOW = -12;
+export const MEDIUM = 0;
+export const HIGH = 12;
--- src/navbar-settings/settings/volume/Volume.js
+++ src/navbar-settings/settings/volume/Volume.js
@@ -8,6 +8,7 @@ import Icon from "_components/icon/Icon";
 import volumeHigh from "_media/settings/volume/volume-high.png";
 import volumeMid from "_media/settings/volume/volume-medium.png";
 import volumeLow from "_media/settings/volume/volume-low.png";
+import { LOW, MEDIUM, HIGH } from "_data/_utils/volume";
 
 const Volume = () => {
   const { volume } = useSelector((state) => state.settings);
@@ -31,27 +32,27 @@ const Volume = () => {
     <section className="Volume">
       <Icon
         icon={volumeLow}
-        otherImgStyles={volume === -12 ? selectedVolume : unselectedVolume}
+        otherImgStyles={volume === LOW ? selectedVolume : unselectedVolume}
         text="LOW"
         onClick={handleVolume}
-        id="-12"
+        id={LOW}
         size="50px"
         width="40px"
       />
       <Icon
         icon={volumeMid}
-        otherImgStyles={volume === 0 ? selectedVolume : unselectedVolume}
+        otherImgStyles={volume === MEDIUM ? selectedVolume : unselectedVolume}
         text="MEDIUM"
         onClick={handleVolume}
-        id="0"
+        id={MEDIUM}
         size="50px"
       />
       <Icon
         icon={volumeHigh}
-        otherImgStyles={volume === 12 ? selectedVolume : unselectedVolume}
+        otherImgStyles={volume === HIGH ? selectedVolume : unselectedVolume}
         text="HIGH"
         onClick={handleVolume}
-        id="12"
+        id={HIGH}
         size="50px"
         width="60px"
       />
