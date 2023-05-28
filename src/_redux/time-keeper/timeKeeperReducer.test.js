b30c7a3f47746c1641ca09050118f9526bc4a975
Adam Thometz <adamthometz@gmail.com>
2023-05-28 13:14:37 -0400

    added tests

--- src/_redux/time-keeper/timeKeeperReducer.test.js
+++ src/_redux/time-keeper/timeKeeperReducer.test.js
@@ -1,8 +1,26 @@
-import timeKeeperReducer, { initialState, toggleTimer, toggleMute, setMillisecondsLeftInSecond } from "./timeKeeperReducer";
+import timeKeeperReducer, { initialState, toggleTimer, toggleMute, setMillisecondsLeftInSecond, addTimeBlocks, decrementOneSecond } from "./timeKeeperReducer";
 
 describe("Time Keeper reducer", () => {
   it("should return the initial state", () => {
     expect(timeKeeperReducer(undefined, {})).toEqual(initialState);
+  });
+
+  it("should add time blocks to timer, 15/30/60 sec", () => {
+    const blockToAdd = { music: "file", seconds: 15, mood: 'excited' };
+    const result = timeKeeperReducer(undefined, addTimeBlocks(blockToAdd));
+    expect(result.song[0]).toEqual(blockToAdd);
+    expect(result.secondsLeft).toEqual(15);
+    expect(result.totalLength).toEqual(15);
+  });
+
+  it("should add time blocks to timer, randomized", () => {
+    // get 30 sec time block every time
+    Math.random = jest.fn(() => 0.5);
+    const blockToAdd = { seconds: 180, mood: 'relaxed' };
+    const result = timeKeeperReducer(undefined, addTimeBlocks(blockToAdd));
+    expect(result.song.length).toBe(6);
+    expect(result.secondsLeft).toEqual(180);
+    expect(result.totalLength).toEqual(180);
   });
 
   it("should toggle playing", () => {
@@ -18,6 +36,13 @@ describe("Time Keeper reducer", () => {
     const result2 = timeKeeperReducer(result, toggleMute());
     expect(result2.isMuted).toBe(false);
   });
+
+  it("should decrement the time by 1 second", () => {
+    const blockToAdd = { seconds: 60, mood: 'relaxed' };
+    const init = timeKeeperReducer(undefined, addTimeBlocks(blockToAdd));
+    const result = timeKeeperReducer(init, decrementOneSecond());
+    expect(result.secondsLeft).toEqual(59);
+  })
   
   it("should set milliseconds", () => {
     const result = timeKeeperReducer(undefined, setMillisecondsLeftInSecond(500));
