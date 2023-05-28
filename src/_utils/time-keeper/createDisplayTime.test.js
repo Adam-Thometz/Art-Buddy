f8f6fd68e4645fbe8cd610fef7866a8693b35fc0
Adam Thometz <adamthometz@gmail.com>
2023-05-28 12:20:47 -0400

    cleanup

--- src/_utils/time-keeper/createDisplayTime.test.js
+++ src/_utils/time-keeper/createDisplayTime.test.js
@@ -2,10 +2,10 @@ import createDisplayTime from "./createDisplayTime.js";
 
 describe("createDisplayTime function", () => {
   const tests = [
-    // assertion   seconds display
-    ["30 seconds", 30,     "00:00:30"],
-    ["3 minutes",  180,    "00:03:00"],
-    ["1 hour, 10 minutes, and 29 seconds", 4229, "01:10:29"],
+    // assertion                           seconds display
+    ["30 seconds",                         30,     "00:00:30"],
+    ["3 minutes",                          180,    "00:03:00"],
+    ["1 hour, 10 minutes, and 29 seconds", 4229,   "01:10:29"],
   ];
   it.each(tests)("display time for %s", (_, seconds, display) => {
     expect(createDisplayTime(seconds)).toBe(display)
--- src/_utils/time-keeper/generateVolume.js
+++ src/_utils/time-keeper/generateVolume.js
@@ -2,7 +2,6 @@ import { LOW, MEDIUM, HIGH } from "_data/_utils/volume";
 
 export default function generateVolume(volume, isMuted) {
   if (isMuted) return 0;
-
   if (volume === LOW) return 0.5;
   if (volume === MEDIUM) return 0.75;
   if (volume === HIGH) return 1;
