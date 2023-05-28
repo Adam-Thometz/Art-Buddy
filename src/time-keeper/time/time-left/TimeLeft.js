4e4672cd0e37227eb5fd3b920727eb502557f39d
Adam Thometz <adamthometz@gmail.com>
2023-05-28 15:07:15 -0400

    added reset and mutefunctions as well as inNav prop

--- src/time-keeper/time/time-left/TimeLeft.js
+++ src/time-keeper/time/time-left/TimeLeft.js
@@ -1,15 +1,54 @@
-import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+import { useSelector, useDispatch } from "react-redux";
+import { toggleMute, resetSong } from "_redux/time-keeper/timeKeeperReducer";
 
 import "./TimeLeft.css";
 
 import createDisplayTime from "_utils/time-keeper/createDisplayTime";
+import reset from "_media/time-keeper/controls/reset.png";
+import mute from "_media/time-keeper/controls/mute.png";
+import urls from "_routes/routeUrls";
 
-const TimeLeft = () => {
-  const { secondsLeft } = useSelector((state) => state.timeKeeper);
+const TimeLeft = ({ inNav }) => {
+  const { secondsLeft, currSongIdx } = useSelector((state) => state.timeKeeper);
+  const dispatch = useDispatch();
+  const navigate = useNavigate()
+
+  const goToTimeKeeper = () => navigate(urls.timeKeeperUrl)
+  const handleMute = () => dispatch(toggleMute());
+  const handleReset = () => {
+    if (currSongIdx === 0) {
+      const audio = document.querySelector(".TimeKeeperNav-audio");
+      if (audio) audio.currentTime = 0;
+    }
+    dispatch(resetSong());
+  }
 
   const timeDisplay = createDisplayTime(secondsLeft);
 
-  return <h1 className="TimeLeft">{timeDisplay}</h1>;
+  return (
+    <div className={`TimeLeft${inNav ? '-small' : ''}`}>
+      <img
+        className="TimeLeft-mute"
+        src={mute}
+        alt=""
+        role="button"
+        onClick={handleMute}
+      />
+      <h1
+        className="TimeLeft-header"
+        onClick={inNav ? goToTimeKeeper : null}
+      >{timeDisplay}</h1>
+      <img
+        className="TimeLeft-reset"
+        src={reset}
+        alt=""
+        role="button"
+        onClick={handleReset}
+      />
+    </div>
+  );
 };
 
 export default TimeLeft;
