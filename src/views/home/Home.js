2ed87faa3b477249942bf19adb5f0173f2ee6d7d
Adam Thometz <adamthometz@gmail.com>
2023-11-25 08:14:10 -0500

    removed unnecessary code

--- src/views/home/Home.js
+++ src/views/home/Home.js
@@ -2,19 +2,12 @@ import useVisited from "hooks/visited/useVisited";
 
 import Welcome from "./welcome/Welcome";
 import Menu from "./menu/Menu";
-import { useEffect } from "react";
 
 const Home = () => {
-  useEffect(() => {
-    async function tryDB() {
-      const response = await(await fetch("http://localhost:8000/instruments")).json();
-      const data = atob(response.$binary.base64);
-      console.log(data);
-    }
-    tryDB()
-  }, []);
   const [hasVisited, setHasVisited] = useVisited();
-  return !hasVisited ? <Welcome setHasVisited={setHasVisited} /> : <Menu />;
+  return hasVisited
+    ? <Menu />
+    : <Welcome setHasVisited={setHasVisited} />;
 };
 
 export default Home;
